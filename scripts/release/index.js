42fe5a3cf8c0d73a66ac122a2e60da4858ac2ec8
youluna <njuzhaoguoyan@foxmail.com>
2018-10-25 17:30:56 +0800

    chore(*): trigger github release event

--- scripts/release/index.js
+++ scripts/release/index.js
@@ -2,59 +2,62 @@ const fs = require('fs-extra');
 const path = require('path');
 const cp = require('child_process');
 const co = require('co');
-const utils = require('../utils');
+const Github = require('@octokit/rest')();
+const inquirer = require('inquirer');
+const { logger, runCmd } = require('../utils');
 const { execSync } = require('child_process');
 
 const cwd = process.cwd();
 const packagePath = path.resolve('package.json');
-let packageInfo;
+const packageInfo = require(packagePath);
 
-const runCmd = function(cmd) {
-    return utils.runCmd(cmd).then(msg => {
-        utils.logger.success(`[command] ${cmd}`, '\n');
-        utils.logger.info(msg);
+const masterTag = packageInfo.version;
+const buildTag = `build/${packageInfo.version}`;
+
+const runCommond = function(cmd) {
+    return runCmd(cmd).then(msg => {
+        logger.success(`[command] ${cmd}`, '\n');
+        logger.info(msg);
     });
 };
 
 co(function* () {
-    packageInfo = require(packagePath);
-
     checkTags();
 
     yield pushMaster();
     yield pushPlatformDocsBranch();
     yield publishToNpm();
 }).catch(err => {
-    utils.logger.error('Release failed', err.stack);
+    logger.error('Release failed', err.stack);
 });
 
 function checkTags() {
     const stdout = execSync('git tag').toString();
     let repeatTag = '';
 
-    if (stdout.match(packageInfo.version)) {
-        repeatTag += `[${packageInfo.version}]`;
+    if (stdout.match(masterTag)) {
+        repeatTag += `[${masterTag}]`;
     }
-    if (stdout.match(`build/${packageInfo.version}`)) {
-        repeatTag += `  [build/${packageInfo.version}]`;
+    if (stdout.match(`${buildTag}`)) {
+        repeatTag += `  [${buildTag}]`;
     }
 
     if (repeatTag !== '') {
-        utils.logger.error(`You have duplicate tags: ${repeatTag}`);
+        logger.error(`You have duplicate tags: ${repeatTag}`);
         process.exit(0);
     } else {
-        utils.logger.success(`There is no [${packageInfo.version}] or [build/${packageInfo.version}] exits`, '\n');
+        logger.success(`There is no [${masterTag}] or [${buildTag}] exits`, '\n');
     }
 }
 
 function* pushMaster() {
-    yield runCmd('git checkout master');
-    yield runCmd('git add .');
-    yield runCmd(`git commit -m 'chore(*): Release-${packageInfo.version}'`);
-    yield runCmd('git pull');
-    yield runCmd('git push origin master');
+    yield runCommond('git checkout master');
+    yield runCommond('git add .');
+    yield runCommond(`git commit -m 'chore(*): Release-${masterTag}'`);
+    yield runCommond('git pull');
+    yield runCommond('git push origin master');
 
-    utils.logger.success('******** push to branch master success! ********\n');
+    logger.success('******** push to branch master success! ********\n');
 }
 
 function* pushPlatformDocsBranch() {
@@ -63,7 +66,7 @@ function* pushPlatformDocsBranch() {
     try {
         yield fs.ensureDir(docs);
 
-        yield runCmd('git worktree add platform-docs -B platform-docs');
+        yield runCommond('git worktree add platform-docs -B platform-docs');
         const currentDir = fs.readdirSync(cwd);
 
         currentDir.forEach(file => {
@@ -79,27 +82,78 @@ function* pushPlatformDocsBranch() {
         const content = fs.readFileSync(buildIgnore, 'utf8');
         fs.writeFileSync(gitIgnore, content);
 
-        yield runCmd('cd platform-docs;git add .');
+        yield runCommond('cd platform-docs;git add .');
 
-        utils.logger.success(`[command] cd platform-docs;git commit -m 'chore(*): Release-${packageInfo.version}-build' || true`);
-        cp.execSync(`cd platform-docs;git commit -m 'chore(*): Release-${packageInfo.version}-build' || true`);
+        logger.success(`[command] cd platform-docs;git commit -m 'chore(*): Release-${buildTag}' || true`);
+        cp.execSync(`cd platform-docs;git commit -m 'chore(*): Release-${buildTag}' || true`);
 
-        yield runCmd('cd platform-docs;git push origin platform-docs -f');
-        yield runCmd(`cd platform-docs;git tag build/${packageInfo.version}`);
-        yield runCmd(`cd platform-docs;git push origin build/${packageInfo.version}`);
+        yield runCommond('cd platform-docs;git push origin platform-docs -f');
+        yield runCommond(`cd platform-docs;git tag ${buildTag}`);
+        yield runCommond(`cd platform-docs;git push origin ${buildTag}`);
 
-        utils.logger.success('******** push to branch platform-docs success! ********\n');
+        logger.success('******** push to branch platform-docs success! ********\n');
     } finally {
         yield fs.remove(docs);
-        yield runCmd('git worktree prune');
+        yield runCommond('git worktree prune');
     }
 }
 
 function* publishToNpm() {
-    yield runCmd('git checkout master');
-    yield runCmd('git pull');
-    yield runCmd(`git tag ${packageInfo.version}`);
-    yield runCmd(`git push origin ${packageInfo.version}`);
-    // yield runCmd('npm publish');
+    yield runCommond('git checkout master');
+    yield runCommond('git pull');
+    yield runCommond(`git tag ${masterTag}`);
+    yield runCommond(`git push origin ${masterTag}`);
+
+    // yield runCommond('npm publish');
+    // yield triggerRelease();
 }
 
+function* getGithubInfo() {
+    return yield inquirer.prompt([{
+        name: 'uname',
+        type: 'input',
+        message: '请输入github用户名:'
+    }, {
+        name: 'pwd',
+        type: 'password',
+        message: '请输入github密码:'
+    }]);
+}
+
+function* triggerRelease() {
+    logger.success(`正在准备发布Github release: ${buildTag}`);
+    const hubInfo = yield getGithubInfo();
+
+    const latestLog = fs.readFileSync(path.join(cwd, 'LATESTLOG.md'), 'utf8')
+        .replace(/\n+/g, '\n')
+        .split('\n').slice(1).join('\n');
+
+    return new Promise(function(resolve, reject) {
+        Github.hook.before('request', () => {
+            Github.authenticate({
+                type: 'basic',
+                username: hubInfo.uname,
+                password: hubInfo.pwd
+            });
+        });
+
+        Github.repos.createRelease({
+            owner: 'alibaba-fusion',
+            repo: 'next',
+            /* eslint-disable */
+            tag_name: buildTag,
+            target_commitish: 'platform-docs',
+            /* eslint-enable */
+            name: buildTag,
+            body: latestLog,
+            draft: false,
+            prerelease: false,
+        }).then(result => {
+            logger.success('Create release success');
+            resolve();
+        }).catch(err => {
+            logger.error(`Failed to release: ${err}`);
+            reject();
+        });
+    });
+}
