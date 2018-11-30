0c9f9ebe81acdf2bb77e21bf6b114dcbf8d2f9a6
jinli.lyy <jinli.lyy@alibaba-inc.com>
2018-11-30 21:42:27 +0800

    feat(Calendar): support rtl

--- src/calendar/head/date-panel-header.jsx
+++ src/calendar/head/date-panel-header.jsx
@@ -4,24 +4,29 @@ import Icon from '../../icon';
 class DatePanelHeader extends React.PureComponent {
 
     render() {
-        const { prefix, visibleMonth, momentLocale, locale, changeMode, goNextMonth, goNextYear, goPrevMonth, goPrevYear } = this.props;
+        const { prefix, rtl, visibleMonth, momentLocale, locale, changeMode, goNextMonth, goNextYear, goPrevMonth, goPrevYear } = this.props;
 
         const localedMonths = momentLocale.months();
         const monthLabel = localedMonths[visibleMonth.month()];
         const yearLable = visibleMonth.year();
         const btnCls = `${prefix}calendar-btn`;
 
+        const leftIcon = rtl ? <Icon type="arrow-right" /> : <Icon type="arrow-left" />;
+        const rightIcon = rtl ? <Icon type="arrow-left" /> : <Icon type="arrow-right" />;
+        const leftDoubleIcon = rtl ? <Icon type="arrow-double-right" /> : <Icon type="arrow-double-left" />;
+        const rightDoubleIcon = rtl ? <Icon type="arrow-double-left" /> : <Icon type="arrow-double-right" />;
+
         return (<div className={`${prefix}calendar-panel-header`}>
             <button
                 role="button"
                 title={locale.prevYear}
                 className={`${btnCls} ${btnCls}-prev-year`}
-                onClick={goPrevYear}><Icon type="arrow-double-left" /></button>
+                onClick={goPrevYear}>{leftDoubleIcon}</button>
             <button
                 role="button"
                 title={locale.prevMonth}
                 className={`${btnCls} ${btnCls}-prev-month`}
-                onClick={goPrevMonth}><Icon type="arrow-left" /></button>
+                onClick={goPrevMonth}>{leftIcon}</button>
             <div className={`${prefix}calendar-panel-header-full`}>
                 <button role="button" className={btnCls} title={monthLabel} onClick={() => changeMode('month', 'start')}>{monthLabel}</button>
                 <button role="button" className={btnCls} title={yearLable} onClick={() => changeMode('year', 'start')}>{yearLable}</button>
@@ -30,12 +35,12 @@ class DatePanelHeader extends React.PureComponent {
                 role="button"
                 title={locale.nextMonth}
                 className={`${btnCls} ${btnCls}-next-month`}
-                onClick={goNextMonth}><Icon type="arrow-right" /></button>
+                onClick={goNextMonth}>{rightIcon}</button>
             <button
                 role="button"
                 title={locale.nextYear}
                 className={`${btnCls} ${btnCls}-next-year`}
-                onClick={goNextYear}><Icon type="arrow-double-right" /></button>
+                onClick={goNextYear}>{rightDoubleIcon}</button>
         </div>);
     }
 }
