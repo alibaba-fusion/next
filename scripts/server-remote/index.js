f1714ea49d2459866ea159c7dcb3b2af5192a865
youluna <njuzhaoguoyan@foxmail.com>
2018-12-03 09:55:59 +0800

    chore(*): add review tool

---
+++ scripts/server-remote/index.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const cp = require('child_process');
+const parseArgs = require('minimist');
+
+const scriptPath = path.join(__dirname, 'server.js');
+
+const argv = parseArgs(process.argv.slice(2));
+
+if (argv._.length === 2 && argv._[0] === 'push') {
+
+    const list = argv._[1].split(',').filter(item => item);
+
+    console.log('1. rm -rf __html =======');
+    cp.execSync('rm -rf __html');
+    console.log('2. git clone http://gitlab.alibaba-inc.com/youlu.zgy/next_html.git __html');
+    cp.execSync('git clone http://gitlab.alibaba-inc.com/youlu.zgy/next_html.git __html');
+    console.log('2. done =======');
+
+    let num = list.length;
+
+    list.forEach(item => {
+        argv.name = item;
+        start(restoreArgs(argv), () => {
+            num--;
+            if (num === 0) {
+                cp.execSync('cd __html/ && git status');
+                cp.execSync('cd __html/ && git add . && git commit -m "html push" && git pull && git push -u -f origin master');
+            }
+        });
+    });
+
+
+    return false;
+}
+
+function start(args, cb) {
+    const worker = cp.fork(scriptPath, args);
+    worker.on('message', data => {
+        if (data === 'RESTART') {
+            worker.kill('SIGINT');
+            start(args);
+        } else if (data.indexOf('CHANGE_LANG') === 0) {
+            worker.kill('SIGINT');
+            const lang = data.split('=')[1];
+            const argv = parseArgs(args);
+            argv.lang = lang;
+            const newArgs = restoreArgs(argv);
+            start(newArgs);
+        } else if (data.indexOf('CHANGE_DIR') === 0) {
+            worker.kill('SIGINT');
+            const dir = data.split('=')[1];
+            const argv = parseArgs(args);
+            argv.dir = dir;
+            const newArgs = restoreArgs(argv);
+            start(newArgs);
+        }
+    });
+
+    worker.on('exit', data => {
+        cb();
+        console.log('========end=====');
+    });
+}
+
+function restoreArgs(argv) {
+    return Object.keys(argv).reduce((ret, key) => {
+        let arr;
+        if (key === '_') {
+            arr = argv._;
+        } else {
+            arr = [`--${key}`, argv[key]];
+        }
+
+        return ret.concat(arr);
+    }, []);
+}
---
+++ scripts/server-remote/loaders/demo/index.js
@@ -0,0 +1,135 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+const ejs = require('ejs');
+const _ = require('lodash');
+const { logger, parseMD, marked, replaceExt } = require('../../../utils');
+
+const selectorPath = require.resolve('./selector');
+
+const cwd = process.cwd();
+const IMPORT_REG = /import {(.+)} from ['"]@alifd\/next['"];?/;
+const IMPORT_LIB_REG = /import (.+) from ['"]@alifd\/next\/lib\/(.+)['"];?/;
+const IMPORT_LIB_REG_G = /^import .+ from ['"]@alifd\/next\/lib\/(.+)['"];?/mg;
+const tplsPath = path.resolve(__dirname, '../../tpls');
+const headerTplPath = path.resolve(tplsPath, 'partials/header.ejs');
+const demoTplPath = path.resolve(tplsPath, 'demo.ejs');
+
+module.exports = function(content) {
+    const options = loaderUtils.getOptions(this);
+    const { dir, lang, links, disableAnimation, componentName } = options;
+    const resourcePath = this.resourcePath;
+    const ext = path.extname(resourcePath);
+    const name = path.basename(resourcePath, ext);
+
+    const scripts = [
+        '../common.js',
+        `./${name}.js`
+        // `/${replaceExt(path.relative(cwd, this.resourcePath), '.js')}`
+    ];
+
+    this.addDependency(headerTplPath);
+    this.addDependency(demoTplPath);
+
+    ejs.renderFile(demoTplPath, {
+        scripts,
+        links,
+        dir,
+        disableAnimation,
+        lang,
+        name
+    }, (err, html) => {
+        if (err) {
+            logger.error(`Render ${name}.html failed: ${err}`);
+        } else {
+            const htmlPath = replaceExt(path.relative(path.join(cwd, 'docs', componentName), this.resourcePath), '.html');
+
+            this.emitFile(htmlPath, html);
+        }
+    });
+
+    const result = parseMD(content, resourcePath, lang, dir);
+    return processJS(result.js, result.css, result.meta.desc, result.body, resourcePath, this.context, dir);
+};
+
+function processJS(js, css, desc, body, resourcePath, context, dir) {
+    if (!js) {
+        return '';
+    }
+
+    js = fixImport(js, resourcePath, dir);
+
+    // eslint-disable-next-line
+  body = marked(body).replace(/`/g, '{backquote}').replace(/\$/g, '{dollar}');
+
+    const hotReloadCode = `
+
+
+// HOT RELOAD CODE
+const componentDesc = document.querySelector('#component-desc');
+componentDesc.innerHTML = \`${desc}\`;
+const componentBody = document.querySelector('#component-body');
+componentBody.innerHTML = \`${body}\`.replace(/{backquote}/g, '\`').replace(/{dollar}/g, '$');
+
+if (module.hot) {
+  module.hot.accept();
+
+  const reloading = document.querySelector('#next-demo-reloading');
+
+  if (!window.hasAddStatusHandler) {
+    module.hot.addStatusHandler(status => {
+      window.hasAddStatusHandler = true;
+      if (status === 'check') {
+        reloading.style.display = 'block';
+      } else if (status === 'idle') {
+        reloading.style.display = 'none';
+      }
+    });
+  }
+}`;
+
+    return `${css ? getCSSRequireString(resourcePath, context) : ''}${js}${hotReloadCode}`;
+}
+
+function getCSSRequireString(resourcePath, context) {
+    const requestString = loaderUtils.stringifyRequest(context, `!!style-loader!css-loader!${selectorPath}!${resourcePath}`);
+    return `require(${requestString})
+
+`;
+}
+
+function fixImport(code, resourcePath) {
+    const matched = code.match(IMPORT_REG);
+    const matchedLib = code.match(IMPORT_LIB_REG_G);
+
+    if (matched) {
+        const components = matched[1].replace(/\s/g, '').split(',');
+
+        const importStrings = components.map(component => {
+            const componentPath = path.join(cwd, 'src', _.kebabCase(component));
+            const relativePath = path.relative(path.dirname(resourcePath), componentPath);
+
+            return `
+import ${component} from '${relativePath}';
+import '${path.join(relativePath, 'style.js')}';
+`;
+        }).join('\n');
+
+        code = code.replace(IMPORT_REG, importStrings);
+    }
+
+    if (matchedLib) {
+        matchedLib.forEach(element => {
+            const component = element.match(IMPORT_LIB_REG)[1].replace(/\s/g, '');
+            const afterLib = element.match(IMPORT_LIB_REG)[2].replace(/\s/g, '');
+            const libPath = path.join(cwd, 'src', afterLib);
+            const newLibPath = path.relative(path.dirname(resourcePath), libPath);
+            const newLibStr = `
+import ${component} from'${newLibPath}'`;
+
+            code = code.replace(IMPORT_LIB_REG, newLibStr);
+        });
+
+    }
+
+    return code;
+}
