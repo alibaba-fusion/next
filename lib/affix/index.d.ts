3e381f90bb051ff57f33f279cd7fe97365c1a110
youluna <njuzhaoguoyan@foxmail.com>
2018-12-04 19:45:29 +0800

    chore(*): Release-build/1.10.2

---
+++ lib/affix/index.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="react" />
+
+import * as React from "react";
+
+export interface AffixProps extends React.HTMLAttributes<HTMLElement> {
+  /**
+   * 设置 Affix 需要监听滚动事件的容器元素
+   */
+  container?: (() => React.ReactElement<any>);
+
+  /**
+   * 距离窗口顶部达到指定偏移量后触发
+   */
+  offsetTop?: number;
+
+  /**
+   * 距离窗口底部达到制定偏移量后触发
+   */
+  offsetBottom?: number;
+
+  /**
+   * 当元素的样式发生固钉样式变化时触发的回调函数
+   */
+  onAffix?: ((元素是否被固钉: boolean) => void);
+}
+
+export default class Affix extends React.Component<AffixProps, any> {}
