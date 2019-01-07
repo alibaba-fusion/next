2ab23f12fe463dd14f03ffa617759c42bbd24bbd
aboutblank <njuzhaoguoyan@foxmail.com>
2019-01-07 18:34:38 +0800

    Revert "fix(Menu): to much data overflow when popupAlign is outside"
    This reverts commit 8cd9e8d4e9ead3f5794964f8466edef2848bab96.

--- src/menu/view/popup-item.jsx
+++ src/menu/view/popup-item.jsx
@@ -96,7 +96,6 @@ export default class PopupItem extends Component {
         }
         if (popupAlign === 'outside' && !(direction === 'hoz' && level === 1)) {
             setStyle(this.popupNode, 'height', `${menuNode.offsetHeight}px`);
-            setStyle(this.popupNode, 'overflow-y', 'scroll');
         }
         // removeClass(this.popupNode, `${prefix}hide`);
 
