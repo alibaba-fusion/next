11fce2b398d24443f98e6533ba2e4271deec5f6a
暮尘 <i@muchen.me>
2019-01-15 17:32:49 +0800

    test(Upload): mod upload test case

--- test/upload/index-spec.js
+++ test/upload/index-spec.js
@@ -14,23 +14,42 @@ const CardUpload = Upload.Card;
 const DragUpload = Upload.Dragger;
 
 const defaultValue = [
-  {
-      name: 'IMG.png',
-      state: 'done',
-      size: 1024,
-      url: 'https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg',
-  }
+    {
+        name: 'IMG.png',
+        state: 'done',
+        size: 1024,
+        url: 'https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg',
+    }
 ];
 
-function fixBinary (bin) {
-    let length = bin.length;
-    let buf = new ArrayBuffer(length);
-    let arr = new Uint8Array(buf);
+function fixBinary(bin) {
+    const length = bin.length;
+    const buf = new ArrayBuffer(length);
+    const arr = new Uint8Array(buf);
     for (let i = 0; i < length; i++) {
-      arr[i] = bin.charCodeAt(i);
+        arr[i] = bin.charCodeAt(i);
     }
     return buf;
-  }
+}
+
+function buildFile(filename = 'test') {
+    const base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
+    const binary = fixBinary(atob(base64));
+    const blob = new Blob([binary], { type: 'image/png' });
+    const file = new File([blob], `${filename}.png`, { type: 'image/png' });
+    return file;
+}
+
+function triggerUploadEvent(wrapper, done, callback) {
+    if (typeof atob === 'function' && typeof Blob === 'function' && typeof File === 'function') {
+        // 模拟文件上传
+        const file = buildFile();
+        wrapper.find('input').simulate('change', { target: { files: [file] } });
+        callback && callback(wrapper);
+    } else {
+        done();
+    }
+}
 
 describe('Upload', () => {
     let requests;
@@ -48,7 +67,7 @@ describe('Upload', () => {
 
     describe('render', () => {
         it('should render a wrapper upload', () => {
-            const wrapper = mount(<Upload listType="text" defaultValue={defaultValue}/>);
+            const wrapper = mount(<Upload listType="text" defaultValue={defaultValue} />);
             assert(wrapper.find('.next-upload').length === 1);
             // remove item
             assert(wrapper.find('.next-upload-list-item').length === 1);
@@ -61,51 +80,44 @@ describe('Upload', () => {
             const wrapper = mount(
                 <Upload
                     action="https://www.easy-mock.com/mock/5b713974309d0d7d107a74a3/alifd/upload"
-                    listType="card" defaultValue={defaultValue}  onChange={(value) => {
-                    assert(value.length === 2);
-                    done();
-                }}/>
+                    listType="card" defaultValue={defaultValue} onChange={(value) => {
+                        assert(value.length === 2);
+                        done();
+                    }} />
             );
 
             assert(wrapper.find('.next-upload-list-item').length === 1);
-
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
+            triggerUploadEvent(wrapper, done, () => {
                 requests[0].respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg"}');
-            } else {
-                done();
-            }
+            });
         });
         it('should support limit', (done) => {
-            const onSelect = (files) => {
-                assert(files.length === 1)
-                done();
-            };
 
-            const wrapper = mount(<Upload autoUplod={false} limit={1} onSelect={onSelect} />);
-
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file, file ] } });
-            } else {
-                done();
+            // limit = 2 上传4个文件 结果应该是 2个成功 2个失败
+            if (!(typeof atob === 'function' && typeof Blob === 'function' && typeof File === 'function')) {
+                return done();
             }
 
+            let success = 0, fail = 0;
+            const isPass = () => {
+                if (success === 2 && fail === 2) {
+                    done();
+                }
+            }
+            const onSuccess = () => {
+                success++;
+                isPass();
+            };
+            const onError = () =>  {
+                fail++;
+                isPass();
+            }
+
+            const files = [1,2,3,4].map(value => buildFile(value));
+
+            const wrapper = mount(<Upload autoUplod={false} limit={2} multiple onSuccess={onSuccess} onError={onError}/>);
+            wrapper.find('input').simulate('change', { target: { files: files } });
+            requests.forEach(req => req.respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg"}'));
         });
 
         it('should support onChange/onRemove events', (done) => {
@@ -120,14 +132,14 @@ describe('Upload', () => {
         });
     });
 
-    describe('[render] drag', ()=> {
+    describe('[render] drag', () => {
         it('should render a drag upload', (done) => {
             const drag = mount(<DragUpload />);
             assert(drag.find('.next-upload-drag').length === 1);
 
-            if (typeof Blob  === 'function' && typeof File === 'function') {
-                let blob = new Blob(['hello'], {type: 'text/plain'});
-                let file = new File([blob], 'test.png', {type: 'image/png'});
+            if (typeof Blob === 'function' && typeof File === 'function') {
+                let blob = new Blob(['hello'], { type: 'text/plain' });
+                let file = new File([blob], 'test.png', { type: 'image/png' });
 
                 drag.find('div.next-upload-inner').simulate('dragover', { dataTransfer: { files: [file] } });
                 drag.find('div.next-upload-inner').simulate('dragleave', { dataTransfer: { files: [file] } });
@@ -168,29 +180,16 @@ describe('Upload', () => {
                 assert(res.test === 123);
                 done();
             }
-            const wrapper = mount(<Upload autoUplod={false} data={{'test-data': 'test-data'}} headers={{'test-head': 'test-head'}}  formatter={formatter}/>);
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
+            const wrapper = mount(<Upload autoUplod={false} data={{ 'test-data': 'test-data' }} headers={{ 'test-head': 'test-head' }} formatter={formatter} />);
+            triggerUploadEvent(wrapper, done, () => {
                 const request = requests[0];
-
                 assert(request.requestHeaders['test-head'] === 'test-head');
                 assert(request.requestBody.get('test-data') === 'test-data');
-
                 requests[0].respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg", "test": 123}');
-            } else {
-                done();
-            }
+            })
         });
         it('should support custom request', done => {
-            const pass = {isPass: false};
+            const pass = { isPass: false };
             const customRequest = function customRequest(options) {
                 pass.isPass = true;
                 return request(options);
@@ -199,98 +198,47 @@ describe('Upload', () => {
                 assert(pass.isPass);
                 done();
             }
-            const wrapper = mount(<Upload autoUplod={false} request={customRequest} onSuccess={onSuccess}/>);
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
-                const request = requests[0];
-
-                request.respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg", "test": 123}');
-            } else {
-                done();
-            }
+            const wrapper = mount(<Upload autoUplod={false} request={customRequest} onSuccess={onSuccess} />);
+            triggerUploadEvent(wrapper, done, () => {
+                requests[0].respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg", "test": 123}');
+            })
         });
         it('should throw error when response json invalid', done => {
 
             const onError = function onError() {
                 done();
             }
-            const wrapper = mount(<Upload autoUplod={false} onError={onError}/>);
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
-                const request = requests[0];
-
-                request.respond(200, {}, '{"succe3}');
-            } else {
-                done();
-            }
+            const wrapper = mount(<Upload autoUplod={false} onError={onError} />);
+            triggerUploadEvent(wrapper, done, () => {
+                requests[0].respond(200, {}, '{"succe3}');
+            });
         });
         it('should throw error when response.success=false', done => {
-
             const onError = function onError() {
                 done();
             }
-            const wrapper = mount(<Upload autoUplod={false} onError={onError}/>);
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
-                const request = requests[0];
-
-                request.respond(200, {}, '{"success": false}');
-            } else {
-                done();
-            }
+            const wrapper = mount(<Upload autoUplod={false} onError={onError} />);
+            triggerUploadEvent(wrapper, done, () => {
+                requests[0].respond(200, {}, '{"success": false}');
+            });
         });
     });
     describe('[behavior] CardUpload Request', () => {
-        it('should supprt header', done => {
+        it('should support header', done => {
 
             const formatter = res => {
                 assert(res.test === 123);
                 done();
             }
-            const wrapper = mount(<CardUpload autoUplod={false} data={{'test-data': 'test-data'}} headers={{'test-head': 'test-head'}}  formatter={formatter}/>);
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
+            const wrapper = mount(<CardUpload autoUplod={false} data={{ 'test-data': 'test-data' }} headers={{ 'test-head': 'test-head' }} formatter={formatter} />);
+            triggerUploadEvent(wrapper, done, () => {
                 const request = requests[0];
 
                 assert(request.requestHeaders['test-head'] === 'test-head');
                 assert(request.requestBody.get('test-data') === 'test-data');
 
                 requests[0].respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg", "test": 123}');
-            } else {
-                done();
-            }
+            });
         });
         it('should support onChange/onRemove events', (done) => {
             const onChange = sinon.spy();
@@ -359,51 +307,36 @@ describe('Upload', () => {
             done();
         });
         it('should support change Data/Action/Headers in BeforeUpload', (done) => {
-            const beforeUpload = () => {};
-
             class App extends React.Component {
                 constructor() {
                     super();
                     this.state = {
                         action: '/upload/files/123',
-                        data: {'test-data': 'test-data'},
-                        headers: {'test-head': 'test-head'},
+                        data: { 'test-data': 'test-data' },
+                        headers: { 'test-head': 'test-head' },
                     };
                 }
 
-                beforeUpload = (file, options) => {
+                beforeUpload = () => {
                     return {
                         action: '/upload/files/beforeUpload',
-                        data: {'test-data': 'beforeUpload'},
-                        headers: {'test-head': 'beforeUpload'},
+                        data: { 'test-data': 'beforeUpload' },
+                        headers: { 'test-head': 'beforeUpload' },
                     };
                 }
                 render() {
-                    return <CardUpload autoUplod={false}  beforeUpload={this.beforeUpload} {...this.state}/>;
+                    return <CardUpload autoUplod={false} beforeUpload={this.beforeUpload} {...this.state} />;
                 }
             };
             const wrapper = mount(<App />);
 
-            if (typeof atob === 'function' && typeof Blob  === 'function' && typeof File === 'function') {
-                // 模拟文件上传
-                let base64 ='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggkFBTzlUWEwwWTRPSHdBQUFBQkpSVTVFcmtKZ2dnPT0=';
-
-                let binary = fixBinary(atob(base64));
-
-                let blob = new Blob([binary], {type: 'image/png'});
-
-                let file = new File([blob], 'test.png', {type: 'image/png'});
-                wrapper.find('input').simulate('change', { target: { files: [ file ] } });
+            triggerUploadEvent(wrapper, done, () => {
                 const request = requests[0];
-
                 assert(request.url === '/upload/files/beforeUpload');
                 assert(request.requestHeaders['test-head'] === 'beforeUpload');
                 assert(request.requestBody.get('test-data') === 'beforeUpload');
-                requests[0].respond(200, {}, '{"success": true, "url":"https://img.alicdn.com/tps/TB19O79MVXXXXcZXVXXXXXXXXXX-1024-1024.jpg", "test": 123}');
                 done();
-            } else {
-                done();
-            }
+            });
         });
     });
 });
