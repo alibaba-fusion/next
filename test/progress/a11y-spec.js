41b1e7b12398afbc3e2b313d7b97f2bb6ef03b47
Justin Kahn <justindavidkahn@gmail.com>
2019-01-25 19:28:55 +0800

    test(Progress): move and update a11y tests - move a11y tests to their own spec file - update tests to use the new a11y util

---
+++ test/progress/a11y-spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Progress from '../../src/progress/index';
+import '../../src/progress/style.js';
+import a11y from '../util/a11y';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+/* eslint-disable no-undef, react/jsx-filename-extension */
+describe('Progress A11y', () => {
+    let wrapper;
+
+    afterEach(() => {
+        if (wrapper) {
+            wrapper.unmount();
+            wrapper = null;
+        }
+        a11y.afterEach();
+    });
+
+    it('should not have any violations for Line Progress', (done) => {
+        wrapper = a11y.test(<Progress percent={30} />, done, { incomplete: true });
+    });
+
+    it('should not have any violations for Circle Progress', (done) => {
+        wrapper = a11y.test(<Progress shape="circle" percent={30} />, done, { incomplete: true });
+    });
+});
