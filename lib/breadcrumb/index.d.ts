045d8430384b19abe5f6ef2213357fd43d40d1fc
jinli.lyy <jinli.lyy@alibaba-inc.com>
2019-01-28 14:22:58 +0800

    chore(*): Release-build/1.12.2

---
+++ lib/breadcrumb/index.d.ts
@@ -0,0 +1,37 @@
+/// <reference types="react" />
+
+import * as React from "react";
+
+export interface ItemProps extends React.HTMLAttributes<HTMLElement> {
+  /**
+   * 面包屑节点链接，如果设置这个属性，则该节点为`<a />` ，否则是`<span />`
+   */
+  link?: string;
+}
+
+export class Item extends React.Component<ItemProps, any> {}
+export interface BreadcrumbProps extends React.HTMLAttributes<HTMLElement> {
+  /**
+   * 样式类名的品牌前缀
+   */
+  prefix?: string;
+
+  /**
+   * 面包屑子节点，需传入 Breadcrumb.Item
+   */
+  children?: any;
+
+  /**
+   * 面包屑最多显示个数，超出部分会被隐藏
+   */
+  maxNode?: number;
+
+  /**
+   * 分隔符，可以是文本或 Icon
+   */
+  separator?: React.ReactNode;
+}
+
+export default class Breadcrumb extends React.Component<BreadcrumbProps, any> {
+  static Item: typeof Item;
+}
