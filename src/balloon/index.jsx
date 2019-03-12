157a233e7908facb3ad22d1b4e3b580aa03e06e4
youluna <njuzhaoguoyan@foxmail.com>
2019-03-12 10:30:13 +0800

    docs(Balloon): change the warning words

--- src/balloon/index.jsx
+++ src/balloon/index.jsx
@@ -2,6 +2,7 @@ import ConfigProvider from '../config-provider';
 import Balloon from './balloon';
 import Tooltip from './tooltip';
 import Inner from './inner';
+import { log } from '../util';
 
 Balloon.Tooltip = ConfigProvider.config(Tooltip, {
     transform: /* istanbul ignore next */ (props, deprecated) => {
@@ -43,10 +44,8 @@ export default ConfigProvider.config(Balloon, {
                 : [props.triggerType];
 
             if (triggerType.indexOf('focus') > -1) {
-                deprecated(
-                    'triggerType[focus]',
-                    'triggerType[hover, click]',
-                    'Balloon'
+                log.warning(
+                    'triggerType[focus] is deprecated at [Balloon], use triggerType[hover, click] instead of it. If focus is desired maybe you should use [Balloon.Tooltip]'
                 );
             }
         }
