51dfaa487465a0998806c0d85d765401a0b7d95c
youluna <njuzhaoguoyan@foxmail.com>
2019-07-15 20:08:31 +0800

    chore(Tag): fix typescripts

--- types/tag/index.d.ts
+++ types/tag/index.d.ts
@@ -4,68 +4,7 @@ import * as React from 'react';
 
 interface HTMLAttributesWeak extends React.HTMLAttributes<HTMLElement> {
     onClick?: any;
-}
-
-export interface CloseableProps extends HTMLAttributesWeak {
-    /**
-     * closeable 标签的 onClose 响应区域, tag: 标签体, tail(默认): 关闭按钮
-     */
-    closeArea?: 'tag' | 'tail';
-
-    /**
-     * 点击关闭按钮时的回调
-     */
-    onClose?: (from: string) => boolean;
-
-    /**
-     * 标签关闭后执行的回调
-     */
-    afterClose?: () => void;
-
-    /**
-     * 点击回调
-     */
-    onClick?: () => void;
-
-    /**
-     * 标签的尺寸（large 尺寸为兼容表单场景 large = medium）
-     */
-    size?: 'small' | 'medium' | 'large';
-}
-
-export class Closeable extends React.Component<CloseableProps, any> {}
-
-interface HTMLAttributesWeak extends React.HTMLAttributes<HTMLElement> {
     onChange?: any;
-}
-
-export interface SelectableProps extends HTMLAttributesWeak {
-    /**
-     * 标签是否被选中，受控用法
-     * tag checked or not, a controlled way
-     */
-    checked?: boolean;
-
-    /**
-     * 标签是否默认被选中，非受控用法
-     * tag checked or not by default, a uncontrolled way
-     */
-    defaultChecked?: boolean;
-
-    /**
-     * 选中状态变化时触发的事件
-     */
-    onChange?: (checked: boolean, e: any) => void;
-
-    /**
-     * 标签是否被禁用
-     */
-    disabled?: boolean;
-}
-
-export class Selectable extends React.Component<SelectableProps, any> {}
-interface HTMLAttributesWeak extends React.HTMLAttributes<HTMLElement> {
-    onClick?: any;
 }
 
 export interface TagProps extends HTMLAttributesWeak {
@@ -100,7 +39,72 @@ export interface TagProps extends HTMLAttributesWeak {
     onClick?: () => void;
 }
 
+export interface CloseableProps extends TagProps {
+    /**
+     * closeable 标签的 onClose 响应区域, tag: 标签体, tail(默认): 关闭按钮
+     */
+    closeArea?: 'tag' | 'tail';
+
+    /**
+     * 点击关闭按钮时的回调
+     */
+    onClose?: (from: string) => boolean;
+
+    /**
+     * 标签关闭后执行的回调
+     */
+    afterClose?: () => void;
+
+    /**
+     * 点击回调
+     */
+    onClick?: () => void;
+
+    /**
+     * 标签的尺寸（large 尺寸为兼容表单场景 large = medium）
+     */
+    size?: 'small' | 'medium' | 'large';
+}
+
+export class Closeable extends React.Component<CloseableProps, any> {}
+
+export interface SelectableProps extends TagProps {
+    /**
+     * 标签是否被选中，受控用法
+     * tag checked or not, a controlled way
+     */
+    checked?: boolean;
+
+    /**
+     * 标签是否默认被选中，非受控用法
+     * tag checked or not by default, a uncontrolled way
+     */
+    defaultChecked?: boolean;
+
+    /**
+     * 选中状态变化时触发的事件
+     */
+    onChange?: (checked: boolean, e: any) => void;
+
+    /**
+     * 标签是否被禁用
+     */
+    disabled?: boolean;
+}
+
+export class Selectable extends React.Component<SelectableProps, any> {}
+
+
+export interface TagGroupProps extends React.HTMLAttributes<HTMLElement> {
+    prefix?: string;
+    rtl?: boolean;
+}
+
+export class TagGroup extends React.Component<TagGroupProps, any> {}
+
+
 export default class Tag extends React.Component<TagProps, any> {
     static Closeable: typeof Closeable;
     static Selectable: typeof Selectable;
+    static Group: typeof TagGroup;
 }
