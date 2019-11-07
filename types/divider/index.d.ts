da1009885c81b6b4a42eb3d1e4c7687c81d819da
youluna <njuzhaoguoyan@foxmail.com>
2019-11-07 19:46:22 +0800

    chore(Divider): add typescript

--- types/divider/index.d.ts
+++ types/divider/index.d.ts
@@ -5,9 +5,17 @@ import CommonProps from '../util';
 
 export interface DividerProps extends React.HTMLAttributes<HTMLElement>, CommonProps {
     /**
-     * children
-     */
-    children?: React.ReactNode;
+    * 是否为虚线
+    */
+    dashed?: boolean;
+    /**
+    * 线是水平还是垂直类型
+    */
+    direction?: 'hoz' | 'ver';
+    /**
+    * 分割线标题的位置
+    */
+    orientation?: 'left' | 'right' | 'center';
 
 }
 
