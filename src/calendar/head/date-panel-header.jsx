68cf1ef8818fbc8b6276ade4db56bf2848fea004
myronliu347 <myronliu347@gmail.com>
2019-11-25 20:47:55 +0800

    temp(*): istanbul ignore

--- src/calendar/head/date-panel-header.jsx
+++ src/calendar/head/date-panel-header.jsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from 'react';
 import Icon from '../../icon';
 import Dropdown from '../../dropdown';
--- src/calendar/head/menu.jsx
+++ src/calendar/head/menu.jsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { findDOMNode } from 'react-dom';
