710c2aa246c0d4a8927da6aa033fd58c6857f9f4
youluna <njuzhaoguoyan@foxmail.com>
2019-12-02 14:57:37 +0800

    fix(*): show avatar & divider in fusion cool

--- docs/divider/adaptor/index.jsx
+++ docs/divider/adaptor/index.jsx
@@ -5,10 +5,31 @@ import { Types } from '@alifd/adaptor-helper';
 export default {
     name: 'Divider',
     editor: () => ({
-
+        props: [{
+            name: 'orientation',
+            type: Types.enum,
+            options: ['center', 'left', 'right'],
+            default: 'center'
+        },
+        {
+            name: 'direction',
+            type: Types.enum,
+            options: ['hoz', 'ver'],
+            default: 'hoz'
+        },
+        {
+            name: 'dashed',
+            type: Types.bool,
+            default: false,
+        },
+        {
+            name: 'text',
+            type: Types.string,
+            default: '',
+        }],
     }),
-    adaptor: ({ level, count, ...others }) => {
-        return <Divider {...others}/>;
+    adaptor: ({ orientation, direction, dashed, text, ...others }) => {
+        return <Divider dashed={dashed} direction={direction} orientation={orientation} children={text} {...others}/>;
     },
     content: () => ({
     })
