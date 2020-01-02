0299695f588d5b81953759b07aa709198e82cb75
youluna <njuzhaoguoyan@foxmail.com>
2020-01-02 13:12:53 +0800

    chore(Balloon): focus can be a trigger type

--- src/balloon/index.jsx
+++ src/balloon/index.jsx
@@ -42,18 +42,6 @@ export default ConfigProvider.config(Balloon, {
             props = { onVisibleChange: newOnVisibleChange, ...others };
         }
 
-        if ('triggerType' in props) {
-            const triggerType = Array.isArray(props.triggerType)
-                ? [...props.triggerType]
-                : [props.triggerType];
-
-            if (triggerType.indexOf('focus') > -1) {
-                log.warning(
-                    'triggerType[focus] is deprecated at [Balloon], use triggerType[hover, click] instead of it. If focus is desired maybe you should use [Balloon.Tooltip]'
-                );
-            }
-        }
-
         return props;
     },
 });
