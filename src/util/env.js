7959ddff7f3c3c79eb9b217c07713d70c8ca83ae
jerryyxu <1193486072@qq.com>
2020-04-03 14:39:19 +0800

    fix(Dialog): get the wrong computed size of borderbox in IE,close #1609

--- src/util/env.js
+++ src/util/env.js
@@ -1,7 +1,7 @@
 /**
  * IE浏览器的渲染引擎版本号
  * 注意：此属性与浏览器版本号不同，IE的渲染引擎版本号是可以通过HTML header或手动设置去更改的
- * @type {Number} 6 ~ 10
+ * @type {Number} 6 ~ 11
  */
 export const ieVersion =
     typeof document !== 'undefined' ? document.documentMode : undefined;
