0a7f1abbd2fc254ec0ddad03b285ebdf2092ae93
youluna <njuzhaoguoyan@foxmail.com>
2020-07-14 10:30:12 +0800

    chore(*): fix config platform mask

--- scripts/docs/configs/webpack/theme/layout.js
+++ scripts/docs/configs/webpack/theme/layout.js
@@ -7,7 +7,8 @@ module.exports = function(data) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 <!-- 外部引用组件自己的样式 (此处将被配置平台替换掉) -->
 <link href="./build/index.css" id="theme-style" rel="stylesheet" type="text/css" />
-<script src="//g.alicdn.com/alife/lib/0.0.11/??react/16.0.0/react.production.min.js,react-dom/16.0.0/react-dom.production.min.js"></script>
+<script src="//g.alicdn.com/code/lib/react/16.10.2/umd/react.production.min.js"></script>
+<script src="//g.alicdn.com/code/lib/react-dom/16.10.2/umd/react-dom.production.min.js"></script>
 <script src="//g.alicdn.com/alife/lib/0.0.11/??moment/2.20.1/moment-with-locales.min.js"></script>
 <script>
 ${data.commonJs}
