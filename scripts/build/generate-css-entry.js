3d2b1879b43679222da6cb18fbf8305211387938
zuoqi <zuoqi.zzq@alibaba-inc.com>
2020-07-23 20:12:39 +0800

    fix: 搞定step

--- scripts/build/generate-css-entry.js
+++ scripts/build/generate-css-entry.js
@@ -12,7 +12,7 @@ const lintCss = require('./lintcss');
 
 module.exports = function() {
     // generate [other components] css variables
-    const componentPaths = glob.sync(path.join(cwd, 'src', '*'));
+    const componentPaths = glob.sync(path.join(cwd, 'src', 'step'));
     componentPaths.forEach(comPath => {
         const componentName = path.basename(comPath);
         if (['demo-helper'].indexOf(componentName) > -1 || !fs.existsSync(path.join(cwd, 'src', componentName, 'style.js'))) {
