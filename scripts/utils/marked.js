23489c0018da4331483c0891a98b4808a0972630
galo.gm <galo.gm.work@gmail.com>
2020-11-18 19:44:31 +0800

    fix(*): add watcher on all with some preview feat added

--- scripts/utils/marked.js
+++ scripts/utils/marked.js
@@ -1,14 +1,20 @@
 const highlight = require('highlight.js');
+const _ = require('lodash');
 const mdIt = require('markdown-it')({
     html: true,
     xhtmlOut: true,
     highlight: function(code, lang) {
         const result =
-            lang && highlight.getLanguage(lang)
-                ? highlight.highlight(lang, code)
-                : highlight.highlightAuto(code);
+            lang && highlight.getLanguage(lang) ? highlight.highlight(lang, code) : highlight.highlightAuto(code);
         return `<div class="hljs">${result.value}</div>`;
     },
+}).use(require('markdown-it-anchor'), {
+    level: 1,
+    slugify: id => _.startCase(id),
+    permalink: true,
+    permalinkClass: 'header-anchor',
+    permalinkSymbol: '#',
+    permalinkBefore: true,
 });
 
 module.exports = function(md) {
