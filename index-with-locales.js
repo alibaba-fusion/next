df9811daf5bbf3564850c947ec3ef153387577ab
hero in my life <799268820@qq.com>
2020-12-16 20:41:47 +0800

    feat(Locale): add pt (#2536)

--- index-with-locales.js
+++ index-with-locales.js
@@ -8,5 +8,6 @@ next.locales['zh-hk'] = require('./lib/locale/zh-hk.js');
 next.locales['zh-tw'] = require('./lib/locale/zh-tw.js');
 next.locales['vi-vn'] = require('./lib/locale/vi-vn.js');
 next.locales['it-it'] = require('./lib/locale/it-it.js');
+next.locales['pt-pt'] = require('./lib/locale/pt-pt.js');
 
 module.exports = next;
