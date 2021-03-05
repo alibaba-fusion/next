b322b3e9802fdbb9b8fb136017931f9356371445
itmajing <itmajing@163.com>
2021-03-05 12:50:39 +0800

    fix(Typescript): update message quick props

--- types/message/index.d.ts
+++ types/message/index.d.ts
@@ -101,8 +101,18 @@ export interface MessageQuickProps extends HTMLAttributesWeak, CommonProps {
      * 是否显示遮罩
      */
     hasMask?: boolean;
+
+    /**
+     * 显示持续时间，0表示一直存在，以毫秒为单位
+     */
     duration?: number;
     timeoutId?: string;
+
+    /**
+     * 显示关闭按钮
+     */
+    closeable?: boolean;
+
     /**
      * 关闭按钮的回调
      */
