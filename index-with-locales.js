0f27ccf79df9a5e3fc20886a6987ed2bddc88abd
youluna <njuzhaoguoyan@foxmail.com>
2021-04-13 17:27:08 +0800

    fix(Locale): ja-ja should be ja-jp

--- index-with-locales.js
+++ index-with-locales.js
@@ -3,6 +3,7 @@ var next = require('./index');
 next.locales = {};
 next.locales['en-us'] = require('./lib/locale/en-us.js');
 next.locales['ja-ja'] = require('./lib/locale/ja-jp.js');
+next.locales['ja-jp'] = require('./lib/locale/ja-jp.js');
 next.locales['zh-cn'] = require('./lib/locale/zh-cn.js');
 next.locales['zh-hk'] = require('./lib/locale/zh-hk.js');
 next.locales['zh-tw'] = require('./lib/locale/zh-tw.js');
