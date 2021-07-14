da7ca9330d6ca3f50284610a8f470dff2ac9dd80
youluna <njuzhaoguoyan@foxmail.com>
2021-07-14 20:23:57 +0800

    chore(*): Release-build/1.23.14

---
+++ es/checkbox/mobile/index.js
@@ -0,0 +1,6 @@
+import { Checkbox as MeetCheckbox } from '@alifd/meet-react';
+import NextCheckbox from '../index';
+
+var Checkbox = MeetCheckbox ? MeetCheckbox : NextCheckbox;
+
+export default Checkbox;
