4a736a43c8f906f4e45542dae267858ba7520ff3
youluna <njuzhaoguoyan@foxmail.com>
2021-07-30 18:22:20 +0800

    chore(*): Release-build/1.23.20

---
+++ es/menu-button/mobile/index.js
@@ -0,0 +1,6 @@
+import { MenuButton as MeetMenuButton } from '@alifd/meet-react';
+import NextMenuButton from '../index';
+
+var MenuButton = MeetMenuButton ? MeetMenuButton : NextMenuButton;
+
+export default MenuButton;
