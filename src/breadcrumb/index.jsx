02fbee4910241e56eea1318f61c504cb18062da2
weinianyang <y13121084245@163.com>
2021-09-18 14:25:21 +0800

    fix(Breadcrumb): breadcrumb add null judge, close #2699

--- src/breadcrumb/index.jsx
+++ src/breadcrumb/index.jsx
@@ -201,61 +201,61 @@ class Breadcrumb extends Component {
             Children.forEach(children, (item, i) => {
                 const ariaProps = {};
 
-                /* 
-                    Breadcrumb没加空值判断，item可能为空                
-                */
-                if (item) {
-                    if (i === length - 1) {
-                        ariaProps['aria-current'] = 'page';
-                    }
+                // 增加空值判断
+                if (!item) {
+                    return;
+                }
+                if (i === length - 1) {
+                    ariaProps['aria-current'] = 'page';
+                }
 
-                    if (i && i === breakpointer) {
-                        items.push(
-                            React.cloneElement(
-                                item,
-                                {
-                                    separator,
-                                    prefix,
-                                    key: i,
-                                    activated: i === length - 1,
-                                    ...ariaProps,
-                                    className: showHiddenItems
-                                        ? `${prefix}breadcrumb-text-ellipsis-clickable`
-                                        : `${prefix}breadcrumb-text-ellipsis`,
-                                },
-                                showHiddenItems ? this.renderEllipsisNodeWithMenu(children, breakpointer) : '...'
-                            )
-                        );
-                    } else if (!i || i > breakpointer) {
-                        items.push(
-                            React.cloneElement(item, {
+                if (i && i === breakpointer) {
+                    items.push(
+                        React.cloneElement(
+                            item,
+                            {
                                 separator,
                                 prefix,
                                 key: i,
-                                ...ariaProps,
                                 activated: i === length - 1,
-                            })
-                        );
-                    }
+                                ...ariaProps,
+                                className: showHiddenItems
+                                    ? `${prefix}breadcrumb-text-ellipsis-clickable`
+                                    : `${prefix}breadcrumb-text-ellipsis`,
+                            },
+                            showHiddenItems ? this.renderEllipsisNodeWithMenu(children, breakpointer) : '...'
+                        )
+                    );
+                } else if (!i || i > breakpointer) {
+                    items.push(
+                        React.cloneElement(item, {
+                            separator,
+                            prefix,
+                            key: i,
+                            ...ariaProps,
+                            activated: i === length - 1,
+                        })
+                    );
                 }
             });
         } else {
             items = Children.map(children, (item, i) => {
                 const ariaProps = {};
-                // 空值判断
-                if (item) {
-                    if (i === length - 1) {
-                        ariaProps['aria-current'] = 'page';
-                    }
-
-                    return React.cloneElement(item, {
-                        separator,
-                        prefix,
-                        activated: i === length - 1,
-                        ...ariaProps,
-                        key: i,
-                    });
+                // 增加空值判断
+                if (!item) {
+                    return;
                 }
+                if (i === length - 1) {
+                    ariaProps['aria-current'] = 'page';
+                }
+
+                return React.cloneElement(item, {
+                    separator,
+                    prefix,
+                    activated: i === length - 1,
+                    ...ariaProps,
+                    key: i,
+                });
             });
         }
 
