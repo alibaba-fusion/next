b45e2ebe33b9ff716657b9ee1f5defa856913ab5
bindoon <bindoon@sina.com>
2021-11-25 11:38:26 +0800

    fix(Dialog): shake while dialog hiding after scrollbar show

--- src/dialog/dialog.jsx
+++ src/dialog/dialog.jsx
@@ -160,7 +160,7 @@ export default class Dialog extends Component {
          */
         centered: PropTypes.bool,
         /**
-         * [v2] 对话框高度超过浏览器视口高度时：打开则显示对话框的滚动条，关闭则撑开页面
+         * [v2] 对话框高度超过浏览器视口高度时，对话框是否展示滚动条。关闭此功后对话框会随高度撑开页面
          * @version 1.25
          */
         overflowScroll: PropTypes.bool,
