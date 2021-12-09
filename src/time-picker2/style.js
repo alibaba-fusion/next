21d6b5d4f2ff3b58bbc1a2ac59b74c286b709b3c
chenxian <lakerswgq@gmail.com>
2021-12-09 19:46:21 +0800

    fix(*): fix sass circular dependency

--- src/time-picker2/style.js
+++ src/time-picker2/style.js
@@ -2,5 +2,6 @@ import '../input/style.js';
 import '../overlay/style.js';
 import '../button/style.js';
 import '../icon/style.js';
-import '../date-picker2/style.js';
+import '../calendar2/style.js';
+import '../date-picker2/main.scss';
 import './main.scss';
