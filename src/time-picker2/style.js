15d0fdbcbdbda5b3a9994369f7e0b24dc662d96f
chenxian <lakerswgq@gmail.com>
2021-12-09 21:19:32 +0800

    fix(TimePicker2): remove unused css

--- src/time-picker2/style.js
+++ src/time-picker2/style.js
@@ -2,6 +2,4 @@ import '../input/style.js';
 import '../overlay/style.js';
 import '../button/style.js';
 import '../icon/style.js';
-import '../calendar2/style.js';
-import '../date-picker2/main.scss';
 import './main.scss';
