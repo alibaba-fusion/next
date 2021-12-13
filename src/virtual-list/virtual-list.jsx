4ff10221a1b9ccd0d5f87c06e8a3801092df87ca
youluna <njuzhaoguoyan@foxmail.com>
2021-12-13 20:12:52 +0800

    fix(VirtualList): fix scroll bar size, close #3600

--- src/virtual-list/virtual-list.jsx
+++ src/virtual-list/virtual-list.jsx
@@ -363,10 +363,10 @@ class VirtualList extends Component {
             return itemSizeGetter(index);
         }
 
-        const height = Object.keys(this.cache)
-            .map(key => this.cache[key])
-            .pop();
-        if (!this.defaultItemHeight && jumpIndex > -1 && height) {
+        if (!this.defaultItemHeight && jumpIndex > -1) {
+            const keysMap = Object.keys(this.cache);
+            const len = keysMap.length;
+            const height = this.cache[len - 1];
             this.defaultItemHeight = height;
         }
 
@@ -402,7 +402,7 @@ class VirtualList extends Component {
 
         const style = { position: 'relative' };
 
-        const size = this.getSpaceBefore(length, this.cacheAdd);
+        const size = this.getSpaceBefore(length, {});
 
         if (size) {
             style.height = size;
