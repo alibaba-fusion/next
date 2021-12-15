b98dc5431150f055a6bb167f55a6df814b5d9e69
尘弦 <lakerswgq@gmail.com>
2021-12-15 11:12:41 +0800

    fix(Tree): fix auto select parent, close #3603 (#3614)

--- src/tree/view/util.js
+++ src/tree/view/util.js
@@ -22,11 +22,7 @@ export function isNodeChecked(node, checkedKeys) {
     if (node.disabled || node.checkboxDisabled) return true;
     /* istanbul ignore next */
     if (node.checkable === false) {
-        return (
-            !node.children ||
-            node.children.length === 0 ||
-            node.children.every(c => isNodeChecked(c, checkedKeys))
-        );
+        return !node.children || node.children.length === 0 || node.children.every(c => isNodeChecked(c, checkedKeys));
     }
     return checkedKeys.indexOf(node.key) > -1;
 }
@@ -53,11 +49,7 @@ export function isNodeDisabledChecked(node) {
     if (node.disabled || node.checkboxDisabled) return true;
     /* istanbul ignore next */
     if (node.checkable === false) {
-        return (
-            !node.children ||
-            node.children.length === 0 ||
-            node.children.every(isNodeDisabledChecked)
-        );
+        return !node.children || node.children.length === 0 || node.children.every(isNodeDisabledChecked);
     }
 
     return false;
@@ -92,12 +84,7 @@ export function filterChildKey(keys, _k2n, _p2n) {
     const newKeys = [];
     keys.forEach(key => {
         const node = getCheckableParentNode(_k2n[key], _p2n);
-        if (
-            !node ||
-            node.checkable === false ||
-            node === _k2n[key] ||
-            keys.indexOf(node.key) === -1
-        ) {
+        if (!node || node.checkable === false || node === _k2n[key] || keys.indexOf(node.key) === -1) {
             newKeys.push(key);
         }
     });
@@ -109,11 +96,7 @@ export function filterParentKey(keys, _k2n) {
 
     for (let i = 0; i < keys.length; i++) {
         const node = _k2n[keys[i]];
-        if (
-            !node.children ||
-            node.children.length === 0 ||
-            node.children.every(isNodeDisabledChecked)
-        ) {
+        if (!node.children || node.children.length === 0 || node.children.every(isNodeDisabledChecked)) {
             newKeys.push(keys[i]);
         }
     }
@@ -154,20 +137,12 @@ export function getAllCheckedKeys(checkedKeys, _k2n, _p2n) {
     checkedKeys = normalizeToArray(checkedKeys);
     const filteredKeys = checkedKeys.filter(key => !!_k2n[key]);
     const flatKeys = [
-        ...filterChildKey(filteredKeys, _k2n, _p2n).filter(
-            key => !(_k2n[key].disabled || _k2n[key].checkboxDisabled)
-        ),
-        ...filteredKeys.filter(
-            key => _k2n[key].disabled || _k2n[key].checkboxDisabled
-        ),
+        ...filterChildKey(filteredKeys, _k2n, _p2n).filter(key => !(_k2n[key].disabled || _k2n[key].checkboxDisabled)),
+        ...filteredKeys.filter(key => _k2n[key].disabled || _k2n[key].checkboxDisabled),
     ];
     const removeKey = child => {
         if (child.disabled || child.checkboxDisabled) return;
-        if (
-            child.checkable === false &&
-            child.children &&
-            child.children.length > 0
-        ) {
+        if (child.checkable === false && child.children && child.children.length > 0) {
             return child.children.forEach(removeKey);
         }
         flatKeys.splice(flatKeys.indexOf(child.key), 1);
@@ -175,25 +150,19 @@ export function getAllCheckedKeys(checkedKeys, _k2n, _p2n) {
 
     const addParentKey = (i, parent) => flatKeys.splice(i, 0, parent.key);
 
+    // 所有 child nodes 选中时，取 parent node，删除所有 child nodes
     const keys = [...flatKeys];
     for (let i = 0; i < keys.length; i++) {
         const pos = _k2n[keys[i]].pos;
         const nums = pos.split('-');
         if (nums.length === 2) {
-            break;
+            continue;
         }
         for (let j = nums.length - 2; j > 0; j--) {
             const parentPos = nums.slice(0, j + 1).join('-');
             const parent = _p2n[parentPos];
-            if (
-                parent.checkable === false ||
-                parent.disabled ||
-                parent.checkboxDisabled
-            )
-                continue;
-            const parentChecked = parent.children.every(child =>
-                isNodeChecked(child, flatKeys)
-            );
+            if (parent.checkable === false || parent.disabled || parent.checkboxDisabled) continue;
+            const parentChecked = parent.children.every(child => isNodeChecked(child, flatKeys));
             if (parentChecked) {
                 parent.children.forEach(removeKey);
                 addParentKey(i, parent);
@@ -203,6 +172,7 @@ export function getAllCheckedKeys(checkedKeys, _k2n, _p2n) {
         }
     }
 
+    // 重新选中所有 child nodes
     const newKeys = [];
     flatKeys.forEach(key => {
         if (_k2n[key].disabled || _k2n[key].checkboxDisabled) {
@@ -229,11 +199,7 @@ export function convertChildren2Data(children) {
 
         return treeNodes
             .map(node => {
-                if (
-                    node.type &&
-                    node.type !== TreeNode &&
-                    node.type.target !== TreeNode
-                ) {
+                if (node.type && node.type !== TreeNode && node.type.target !== TreeNode) {
                     // 为了兼容之前的实现 保留非法节点
                     return {
                         illegalFlag: true,
@@ -247,10 +213,7 @@ export function convertChildren2Data(children) {
 
                 const nodeData = { key, ...rest };
 
-                if (
-                    children &&
-                    !(Array.isArray(children) && !children.length)
-                ) {
+                if (children && !(Array.isArray(children) && !children.length)) {
                     nodeData.children = convert(children);
                 }
 
