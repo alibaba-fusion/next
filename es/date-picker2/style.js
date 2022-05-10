ba8e085aa587da4b8a7e6af4b66b04aba60ebf10
chenxian <lakerswgq@gmail.com>
2022-05-10 14:47:55 +0800

    chore(*): Release-build/1.25.35

---
+++ es/date-picker2/style.js
@@ -0,0 +1,7 @@
+import '../input/style.js';
+import '../overlay/style.js';
+import '../calendar2/style.js';
+import '../time-picker2/style.js';
+import '../button/style.js';
+import '../icon/style.js';
+import './main.scss';
