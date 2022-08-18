2a3b29b699405bb70152c2beebe570fb931a6f58
chenxian <lakerswgq@gmail.com>
2022-08-18 14:33:52 +0800

    chore(*): fix merge conflict

--- src/dialog/dialog-v2.jsx
+++ src/dialog/dialog-v2.jsx
@@ -42,7 +42,6 @@ const Dialog = props => {
         animation = { in: 'fadeInUp', out: 'fadeOutUp' },
         cache,
         wrapperStyle,
-        wrapperClassName,
         popupContainer = document.body,
         dialogRender,
         centered,
