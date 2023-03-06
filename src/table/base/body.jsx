d64057aca5798b08643b315deb02809d5a309da1
weinianyang <ywn01187229@alibaba-inc.com>
2023-03-06 15:00:13 +0800

    fix(Table): fix table dataSource is null || undefined report errors, close #4070

--- src/table/base/body.jsx
+++ src/table/base/body.jsx
@@ -156,7 +156,7 @@ export default class Body extends React.Component {
                 </table>
             );
         }
-        if (dataSource.length) {
+        if (dataSource && dataSource.length && dataSource.indexOf(undefined || null) === -1) {
             rows = dataSource.map((record, index) => {
                 let rowProps = {};
                 // record may be a string
