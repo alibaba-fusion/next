e9e956daacdc804d5bc8e529d700c057faa98aa9
Guo Yunhe <guoyunhe.gyh@alibaba-inc.com>
2023-05-04 16:35:18 +0800

    fix(Button): add types support for react-router Link as component (#4329)

--- types/button/index.d.ts
+++ types/button/index.d.ts
@@ -52,8 +52,9 @@ export interface ButtonProps extends HTMLAttributesWeak, CommonProps {
 
     /**
      * 设置标签类型
+     * TODO: 这里的 ReactNode 是错的，但是为了向前兼容而保留，下个大版本应该去掉
      */
-    component?: 'button' | 'a' | React.ReactNode;
+    component?: 'button' | 'a' | React.ReactNode | React.ComponentType;
 
     /**
      * 设置按钮的载入状态
@@ -94,6 +95,11 @@ export interface ButtonProps extends HTMLAttributesWeak, CommonProps {
      * 在Button组件使用component属性值为a时有效，代表何处打开链接文档
      */
     target?: string;
+
+    /**
+     * 在Button组件使用component属性值为 React Router Link 时有效，代表链接页面的路径
+     */
+    to?: string;
 }
 
 export default class Button extends React.Component<ButtonProps, any> {
