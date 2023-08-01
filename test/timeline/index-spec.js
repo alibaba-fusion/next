3423912bde84c33253c6d934a1e8a112395a802a
chenxian <lakerswgq@gmail.com>
2023-08-01 18:09:53 +0800

    fix(Timeline): fix i18n issue, close #4398

--- test/timeline/index-spec.js
+++ test/timeline/index-spec.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import assert from 'power-assert';
+import ConfigProvider from '../../src/config-provider';
 import Timeline from '../../src/timeline/index';
+import enUS from '../../src/locale/en-us';
 import '../../src/timeline/style.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const Item = Timeline.Item;
+
+const TimelineItem = Item;
 
 /* eslint-disable */
 describe('Timeline', () => {
@@ -86,6 +90,43 @@ describe('Timeline', () => {
             assert(wrapper.find('.next-timeline-item-left').length === wrapper.find('.next-timeline-item-right').length);
         });
     });
+
+    describe('i18n', () => {
+        let wrapper, parent;
+
+        beforeEach(() => {
+            parent = document.createElement('div');
+            document.body.appendChild(parent);
+        });
+
+        afterEach(() => {
+            wrapper.unmount();
+            document.body.removeChild(parent);
+            parent = null;
+            wrapper = null;
+        });
+
+
+        it('should support i18n using ConfigProvider', () => {
+            wrapper = mount(
+                <ConfigProvider locale={enUS}>
+                    <Timeline  fold={[{ foldArea: [1, 2], foldShow: false }, { foldArea: [5], foldShow: false }]}>
+                        <TimelineItem title="Sign" content="[Hangzhou] has received the sign, the signer is Alibaba Post Office, thank you for using STO, looking forward to serving you again" time={'2016-06-10 10:30:00'} state="process" />
+                        <TimelineItem title="Delivery" content="[Hangzhou]Express has reached Hangzhou Hangzhou Binjiang Company" time={'2016-06-10 09:30:00'} />
+                        <TimelineItem title="Delivery" content="[Hangzhou] Zhejiang Hangzhou Binjiang Company dispatches members for you" time={'2016-06-10 09:03:00'} />
+                        <TimelineItem title="Transport" content="[Hangzhou] Zhejiang Hangzhou Transshipment Center has been issued" time={'2016-06-10 06:10:00'} />
+                        <TimelineItem title="Transport" content="[Dongguan City] Guangdong Dongguan Transshipment Center has been issued" time={'2016-06-09 18:00:00'} />
+                        <TimelineItem title="Processing" content="[Dongguan City] Shentong Express Guangdong Fenggang Branch of Guangdong Province" time={'2016-06-09 16:12:00'} />
+                        <TimelineItem title="Processing" content="[Dongguan City] The merchant is informing the express company to insert" time={'2016-06-09 14:00:00'} />
+                        <TimelineItem title="Processing" content="[Dongguan City] Your order to be picked" time={'2016-06-09 10:12:19'} />
+                        <TimelineItem title="Processing" content="[Dongguan] Your order starts processing" time={'2016-06-09 10:01:09'} />
+                    </Timeline>
+                </ConfigProvider>,
+            { attachTo: parent }
+            );
+            assert(wrapper.find('.next-btn-helper').at(0).text() === 'Expand');
+        });
+    })
 
     describe('action', () => {
         let wrapper, parent;
