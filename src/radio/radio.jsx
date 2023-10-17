3eac44dedd60b8da13fc31795e043bee28be6a7e
gogocrow <103985981+gogocrow@users.noreply.github.com>
2023-10-16 21:46:20 -0500

    feat(Radio): add focus support for RadioGroup (#4465)
    Co-authored-by: 周鲂 <jiangqi.jiangqi@alibaba-inc.com>

--- src/radio/radio.jsx
+++ src/radio/radio.jsx
@@ -112,6 +112,8 @@ class Radio extends UIState {
 
         this.state = { checked };
 
+        this.radioRef = null;
+
         this.onChange = this.onChange.bind(this);
     }
 
@@ -173,6 +175,13 @@ class Radio extends UIState {
         }
     }
 
+    focus() {
+        if (this.radioRef) {
+            this.radioRef.focus();
+            this._onUIFocus();
+        }
+    }
+
     render() {
         /* eslint-disable no-unused-vars */
         const {
@@ -217,7 +226,6 @@ class Radio extends UIState {
                 </p>
             );
         }
-
         let input = (
             <input
                 {...obj.pickOthers(othersData, others)}
@@ -230,6 +238,9 @@ class Radio extends UIState {
                 onChange={this.onChange}
                 aria-checked={checked}
                 className={`${prefix}radio-input`}
+                ref={e => {
+                    this.radioRef = e;
+                }}
             />
         );
 
