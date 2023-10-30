a7e1480ad4ab10781d1789f528c3a177a39fad01
珵之 <chengzhi.zpc@alibaba-inc.com>
2023-10-30 11:16:42 +0800

    fix(Switch): should unClickable while diabled, close #4478

--- test/switch/index-spec.js
+++ test/switch/index-spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
@@ -25,6 +26,18 @@ describe('Switch', () => {
         assert(wrapper.find('.next-switch-on').length === 1);
         wrapper.find('.next-switch').simulate('click');
         assert(wrapper.find('.next-switch-on').length === 1);
+    });
+    it('should unClickable while disabled', () => {
+        let clicked = false;
+        const wrapper = mount(<Switch disabled onClick={() => (clicked = true)} />);
+        const el = wrapper.getDOMNode();
+        assert(el);
+        ReactTestUtils.Simulate.click(el);
+        assert(!clicked);
+        wrapper.setProps({ disabled: false });
+        ReactTestUtils.Simulate.click(el);
+        assert(clicked);
+        wrapper.unmount();
     });
     it('children setting and onChange', () => {
         class App extends React.Component {
@@ -59,13 +72,7 @@ describe('Switch', () => {
         assert(wrapper.find('.next-switch-children').text() === '开');
         //非受控的onChange
         const onChange = sinon.spy();
-        const wrapper1 = mount(
-            <Switch
-                checkedChildren="开"
-                unCheckedChildren="关"
-                onChange={onChange}
-            />
-        );
+        const wrapper1 = mount(<Switch checkedChildren="开" unCheckedChildren="关" onChange={onChange} />);
         assert(wrapper1.find('.next-switch-children').text() === '关');
         wrapper1.find('.next-switch').simulate('click');
         assert(wrapper1.find('.next-switch-children').text() === '开');
@@ -125,35 +132,17 @@ describe('Switch', () => {
         assert(wrapper.find('.next-switch-off').length === 1);
     });
     it('should renderPreview', () => {
-        const wrapper = mount(
-            <Switch
-                id="render-preview"
-                isPreview
-                renderPreview={() => 'preview switch'}
-            />
-        );
+        const wrapper = mount(<Switch id="render-preview" isPreview renderPreview={() => 'preview switch'} />);
 
         assert(wrapper.getDOMNode().innerText === 'preview switch');
     });
     it('should use checkedChildren if exist when on & preview', () => {
-        const wrapper = mount(
-            <Switch
-                isPreview
-                checked
-                checkedChildren="✓"
-            />
-        );
+        const wrapper = mount(<Switch isPreview checked checkedChildren="✓" />);
 
         assert(wrapper.getDOMNode().innerText === '✓');
     });
     it('should use unCheckedChildren if exist when off & preview', () => {
-        const wrapper = mount(
-            <Switch
-                isPreview
-                checked={false}
-                unCheckedChildren="✕"
-            />
-        );
+        const wrapper = mount(<Switch isPreview checked={false} unCheckedChildren="✕" />);
 
         assert(wrapper.getDOMNode().innerText === '✕');
     });
