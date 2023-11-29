a11365a000a09a9844bd6f2dc69950392a174a29
KeXi <121713235+kyokaxin@users.noreply.github.com>
2023-11-29 23:55:53 +0800

    fix(Table): header and body always scroll together while GroupHeader and StickyLock, close 4396 (#4550)
    * fix(Table): should support Header and Body follow the TableGroupHeader when it locks the columns
    
    * fix(Table): delete useless code
    
    * fix(Table): adjust test spec for #4396
    
    ---------
    
    Co-authored-by: 珵之 <chengzhi.zpc@alibaba-inc.com>

--- src/table/list/body.jsx
+++ src/table/list/body.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
+import PropTypes from 'prop-types';
 import BodyComponent from '../base/body';
 
-export default function ListBody(props) {
-    return <BodyComponent component="div" {...props} />;
+export default class ListBody extends React.Component {
+    static contextTypes = {
+        getNode: PropTypes.func,
+        onFixedScrollSync: PropTypes.func,
+    };
+
+    componentDidMount() {
+        const { getNode } = this.context;
+        getNode && getNode('body', findDOMNode(this));
+    }
+
+    onScroll = e => {
+        const { onFixedScrollSync } = this.context;
+        onFixedScrollSync && onFixedScrollSync(e);
+    };
+
+    render() {
+        return <BodyComponent component="div" onScroll={this.onScroll} {...this.props} />;
+    }
 }
