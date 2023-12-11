f452cca8fb1499c0d43dbe9e1580f969932cea01
珵之 <chengzhi.zpc@alibaba-inc.com>
2023-12-11 22:46:49 +0800

    chore(BuildTool): adjust project config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            name: 'my-polyfill',
+            config() {
+                return {
+                    resolve: {
+                        alias: [
+                            {
+                                find: /^moment$/,
+                                replacement: 'moment/moment.js',
+                            },
+                        ],
+                    },
+                };
+            },
+        },
+    ],
 });
