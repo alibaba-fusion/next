fcdb5a3241db67ea123776788b3bdf6aa52dcbda
珵之 <chengzhi.zpc@alibaba-inc.com>
2023-12-17 19:29:56 +0800

    chore(Button): adjust test specs

--- components/button/__tests__/a11y-spec.tsx
+++ components/button/__tests__/a11y-spec.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Button from '../../src/button/index';
-import '../../src/button/style';
-import { unmount, testReact } from '../util/a11y/validate';
+import * as React from 'react';
+import Button from '../index';
+import '../style';
+import { testReact } from '../../util/__tests__/a11y/validate';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-/* eslint-disable no-undef, react/jsx-filename-extension */
 describe('Button A11y', () => {
-    let wrapper;
-
-    afterEach(() => {
-        if (wrapper) {
-            wrapper.unmount();
-            wrapper = null;
-        }
-        unmount();
-    });
-
     it('should not have any violations when default', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Button type="normal">Normal</Button> &nbsp;&nbsp;
                 <Button type="normal" disabled>
@@ -38,11 +23,10 @@ describe('Button A11y', () => {
                 </Button>
             </div>
         );
-        return wrapper;
     });
 
     it('should not have any violations when text buttons', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Button type="normal" text>
                     Normal
@@ -70,11 +54,10 @@ describe('Button A11y', () => {
                 </Button>
             </div>
         );
-        return wrapper;
     });
 
     it('should not have any violations when warning buttons', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Button type="normal" warning>
                     Normal
@@ -94,21 +77,19 @@ describe('Button A11y', () => {
                 &nbsp;&nbsp;
             </div>
         );
-        return wrapper;
     });
 
     it('should not have any violations when anchor tag', async () => {
         const props = {
-            component: 'a',
+            component: 'a' as const,
             href: 'http://www.alibaba.com',
             target: '_blank',
         };
-        wrapper = await testReact(<Button {...props}>alibaba</Button>);
-        return wrapper;
+        await testReact(<Button {...props}>alibaba</Button>);
     });
 
     it('should not have any violations when ghost', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Button ghost="light">Ghost light</Button> &nbsp;&nbsp;
                 <Button ghost="light" disabled>
@@ -121,30 +102,27 @@ describe('Button A11y', () => {
                 </Button>
             </div>
         );
-        return wrapper;
     });
 
     it('should not have any violations when in a Button Group', async () => {
-        wrapper = await testReact(
+        await testReact(
             <Button.Group>
                 <Button type="primary">OK</Button>
                 <Button type="secondary">Cancel</Button>
             </Button.Group>
         );
-        return wrapper;
     });
 
     it('should not have any violations when loading', async () => {
-        wrapper = await testReact(
+        await testReact(
             <Button type="secondary" loading>
                 Loading
             </Button>
         );
-        return wrapper;
     });
 
     it('should not have any violations when various sizes', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Button type="primary" size="large">
                     Large
@@ -163,6 +141,5 @@ describe('Button A11y', () => {
                 </Button.Group>
             </div>
         );
-        return wrapper;
     });
 });
