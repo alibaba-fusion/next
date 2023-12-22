e328c236cb2e1b8f2056680b264ec4e1a637eee9
JackLian <jack.lianjie@gmail.com>
2023-12-22 15:26:00 +0800

    refactor(Paragraph): convert paragraph to TypeScript, impove docs and tests

--- components/paragraph/mobile/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Paragraph as MeetParagraph } from '@alifd/meet-react';
-import NextParagraph from '../index';
-
-const Paragraph = MeetParagraph ? MeetParagraph : NextParagraph;
-
-export default Paragraph;
