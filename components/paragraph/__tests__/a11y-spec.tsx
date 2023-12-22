fd88949515e5649c0f28cf20273d3f315021a39b
JackLian <jack.lianjie@gmail.com>
2023-12-22 15:04:33 +0800

    refactor(Paragraph): convert paragraph to TypeScript, impove docs and tests

--- components/paragraph/__tests__/a11y-spec.tsx
+++ components/paragraph/__tests__/a11y-spec.tsx
@@ -1,40 +1,24 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as React from 'react';
 import Paragraph from '../index';
 import '../style';
-import { unmount, testReact } from '../../util/__tests__/legacy/a11y/validate';
-
-Enzyme.configure({ adapter: new Adapter() });
+import { testReact } from '../../util/__tests__/a11y/validate';
 
 const content =
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-/* eslint-disable no-undef, react/jsx-filename-extension */
 describe('Paragraph A11y', () => {
-    let wrapper;
-
-    afterEach(() => {
-        if (wrapper) {
-            wrapper.unmount();
-            wrapper = null;
-        }
-        unmount();
-    });
-
     it('should not have any violations', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Paragraph>{content}</Paragraph>
                 <br />
                 <Paragraph size="small">{content}</Paragraph>
             </div>
         );
-        return wrapper;
     });
 
     it('should not have any violations when type=`short`', async () => {
-        wrapper = await testReact(
+        await testReact(
             <div>
                 <Paragraph type="short">{content}</Paragraph>
                 <br />
@@ -43,6 +27,5 @@ describe('Paragraph A11y', () => {
                 </Paragraph>
             </div>
         );
-        return wrapper;
     });
 });
