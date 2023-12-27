d3f6a440504b3d305a71a81b7f8a5a192ce88b37
JackLian <jack.lianjie@gmail.com>
2023-12-27 09:00:02 +0800

    fix(Paragraph): fix cr issues

--- components/paragraph/mobile/index.tsx
+++ components/paragraph/mobile/index.tsx
@@ -1,3 +1,7 @@
-import Paragraph from '../index';
+// @ts-expect-error meet-react does not export Paragraph
+import { Paragraph as MeetParagraph } from '@alifd/meet-react';
+import NextParagraph from '../index';
+
+const Paragraph = MeetParagraph ? MeetParagraph : NextParagraph;
 
 export default Paragraph;
