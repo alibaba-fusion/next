1023a8e8c3cb260279ba2da98738dd8d35a2eee3
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-01-03 19:33:02 +0800

    chore(BuildTool): rm types, it will be generated when build

--- types/util/date.d.ts
+++
@@ -1,8 +0,0 @@
-import * as dayjs from 'dayjs';
-import 'dayjs/locale/zh-cn';
-type _dayjs = typeof dayjs;
-export interface dayjsWithIsSelf extends _dayjs {
-    isSelf: _dayjs['isDayjs'];
-}
-declare const datejs: dayjsWithIsSelf;
-export default datejs;
--- types/util/pick-attrs.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 过滤掉无效的透传到 DOM 属性
- */
-declare const _default: <V = unknown>(props: Record<string, V>) => Record<string, V>;
-export default _default;
