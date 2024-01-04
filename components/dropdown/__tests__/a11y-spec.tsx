edc7c118d54353706196c060de0ecd86b0a94cd5
暁仙 <xiaoxian.xlf@alibaba-inc.com>
2024-01-04 11:41:56 +0800

    refactor(Dropdown): improve ts & docs & test

---
+++ components/dropdown/__tests__/a11y-spec.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Dropdown from '../index';
+import '../style';
+import { test, createContainer, testReact } from '../../util/__tests__/a11y/validate';
+
+const portalContainerId = 'a11y-portal-id';
+describe('Dropdown A11y', () => {
+    it('should not have any violations', async () => {
+      const portalContainer: HTMLElement = createContainer(portalContainerId);
+        await testReact(
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <Dropdown trigger={<a>Hello dropdown</a>} visible container={portalContainer}>
+                <div>dropdown</div>
+            </Dropdown>
+        );
+        return test(portalContainer);
+    });
+});
