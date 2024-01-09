77b6a42d6578a67346c00ede8f7c6233c911cbbb
xuanji.w <xuanji.w@alibaba-inc.com>
2024-01-09 12:42:40 +0800

    refactor(List): convert to TypeScript, impove docs and tests

--- components/list/__tests__/a11y-spec.tsx
+++ components/list/__tests__/a11y-spec.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import List from '../index';
 import '../style';
-import { unmount, testReact } from '../../util/__tests__/legacy/a11y/validate';
+import { testReact } from '../../util/__tests__/a11y/validate';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-/* eslint-disable no-undef, react/jsx-filename-extension */
 describe('List A11y', () => {
-    let wrapper;
-
-    afterEach(() => {
-        if (wrapper) {
-            wrapper.unmount();
-            wrapper = null;
-        }
-        unmount();
-    });
-
     it('should render', async () => {
-        wrapper = await testReact(<List />);
-        return wrapper;
+        await testReact(<List />);
     });
 });
