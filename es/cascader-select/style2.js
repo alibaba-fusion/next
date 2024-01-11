6d7f717cab6c2121763f05981d839872ee6c69ad
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-01-11 14:51:31 +0800

    chore(*): Release-build/1.27.2

---
+++ es/cascader-select/style2.js
@@ -0,0 +1,3 @@
+import '../select/style2';
+import '../cascader/style2';
+import './index.css';
---
+++ lib/util/types.d.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+import { ComponentCommonProps } from '../config-provider/types';
+export interface IconsType {
+    [key: string]: ReactNode;
+}
+export interface CommonProps extends ComponentCommonProps {
+}
