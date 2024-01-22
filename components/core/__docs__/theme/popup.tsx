8fcd981ba851a942ad8960cdfa9fb9058bb7ca4c
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-01-22 20:35:01 +0800

    chore(Core): rename to ts

--- components/core/__docs__/theme/popup.tsx
+++ components/core/__docs__/theme/popup.tsx
@@ -11,11 +11,11 @@ const { SubMenu, Item, Divider } = Menu;
 class PopupDemo extends React.Component {
     render() {
         const tdStyle = {
-            verticalAlign: 'top'
+            verticalAlign: 'top',
         };
 
         const style = {
-            width: '200px'
+            width: '200px',
         };
         const popupContainer = trigger => trigger.parentNode;
 
@@ -33,7 +33,13 @@ class PopupDemo extends React.Component {
                             </Menu>
                         </td>
                         <td style={tdStyle}>
-                            <TreeSelect style={style} visible treeDefaultExpandAll popupContainer={popupContainer} popupClassName="next-popup">
+                            <TreeSelect
+                                style={style}
+                                visible
+                                treeDefaultExpandAll
+                                popupContainer={popupContainer}
+                                popupClassName="next-popup"
+                            >
                                 <TreeSelect.Node key="1" label="Node 1" />
                                 <TreeSelect.Node key="2" label="Node 2">
                                     <TreeSelect.Node key="2-1" label="Node 2-1" />
@@ -42,20 +48,26 @@ class PopupDemo extends React.Component {
                             </TreeSelect>
                         </td>
                         <td style={tdStyle}>
-                        <Menu style={style} mode="popup" openKeys={['sub-1']}>
-                            <Item key="1">Option 1</Item>
-                            <Item key="2">Option 2</Item>
-                            <Item key="3">Option 3<div style={{display: 'none'}} className="next-popup next-menu-spacing-lr"></div></Item>
-                            <Divider key="divider" />
-                            <SubMenu key="sub-1" className="next-popup" label="Popup menu 1">
-                                <Item key="popup-1-1">Popup option 1</Item>
-                                <Item key="popup-1-2">Popup option 2</Item>
-                            </SubMenu>
-                            <SubMenu key="sub-2" label="Popup menu 2">
-                                <Item key="popup-2-1">Popup option 1</Item>
-                                <Item key="popup-2-2">Popup option 2</Item>
-                            </SubMenu>
-                        </Menu>
+                            <Menu style={style} mode="popup" openKeys={['sub-1']}>
+                                <Item key="1">Option 1</Item>
+                                <Item key="2">Option 2</Item>
+                                <Item key="3">
+                                    Option 3
+                                    <div
+                                        style={{ display: 'none' }}
+                                        className="next-popup next-menu-spacing-lr"
+                                    ></div>
+                                </Item>
+                                <Divider key="divider" />
+                                <SubMenu key="sub-1" className="next-popup" label="Popup menu 1">
+                                    <Item key="popup-1-1">Popup option 1</Item>
+                                    <Item key="popup-1-2">Popup option 2</Item>
+                                </SubMenu>
+                                <SubMenu key="sub-2" label="Popup menu 2">
+                                    <Item key="popup-2-1">Popup option 1</Item>
+                                    <Item key="popup-2-2">Popup option 2</Item>
+                                </SubMenu>
+                            </Menu>
                         </td>
                     </tr>
                 </tbody>
@@ -69,13 +81,16 @@ window.renderDemo = function () {
         <Demo title="Popup">
             <tr>
                 <td style={{ height: '40px', fontSize: '14px', paddingLeft: '20px' }}>
-                    配置该浮层的 Shadow\Corner\Border，会涉及到联动的组件一起改变的有：Menu、TreeSelect、CascaderSelect、TimePicker、DatePicker
+                    配置该浮层的
+                    Shadow\Corner\Border，会涉及到联动的组件一起改变的有：Menu、TreeSelect、CascaderSelect、TimePicker、DatePicker
                 </td>
             </tr>
             <DemoGroup label={false}>
                 <PopupDemo />
             </DemoGroup>
-        </Demo>, document.getElementById('container'));
+        </Demo>,
+        document.getElementById('container')
+    );
 };
 
 renderDemo();
