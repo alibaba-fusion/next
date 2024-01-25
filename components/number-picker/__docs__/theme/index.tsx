90fa0c1f75a8d11b141fd4163768875e7efed720
骆林 <luolin.ck@alibaba-inc.com>
2024-01-25 16:48:06 +0800

    refactor(NumberPicker): rename to ts

---
+++ components/number-picker/__docs__/theme/index.tsx
@@ -0,0 +1,47 @@
+import '../../../demo-helper/style';
+import { Demo, DemoGroup, DemoHead, initDemo } from '../../../demo-helper';
+import NumberPicker from '../../index';
+import '../../style';
+
+// import demo helper
+
+// import number-picker
+
+ReactDOM.render(
+    <div className="demo-container">
+        <Demo title="Number Picker" block>
+            <Demo title="Normal" block>
+                <DemoHead cols={['Large', 'Medium', 'Small']} />
+
+                <DemoGroup label="Normal">
+                    <NumberPicker defaultValue={0} size="large" />
+                    <NumberPicker defaultValue={0} />
+                    <NumberPicker defaultValue={0} size="small" />
+                </DemoGroup>
+                <DemoGroup label="Disabled">
+                    <NumberPicker defaultValue={0} size="large" disabled />
+                    <NumberPicker defaultValue={0} disabled />
+                    <NumberPicker defaultValue={0} size="small" disabled />
+                </DemoGroup>
+            </Demo>
+
+            <Demo title="Inline" block>
+                <DemoHead cols={['Large', 'Medium']} />
+
+                <DemoGroup label="Normal">
+                    <NumberPicker defaultValue={0} size="large" type="inline" />
+                    <NumberPicker defaultValue={0} type="inline" />
+                    <NumberPicker defaultValue={0} size="small" type="inline" />
+                </DemoGroup>
+                <DemoGroup label="Disabled">
+                    <NumberPicker defaultValue={0} size="large" type="inline" disabled />
+                    <NumberPicker defaultValue={0} type="inline" disabled />
+                    <NumberPicker defaultValue={0} size="small" type="inline" disabled />
+                </DemoGroup>
+            </Demo>
+        </Demo>
+    </div>,
+    document.getElementById('container')
+);
+
+initDemo('number-picker');
