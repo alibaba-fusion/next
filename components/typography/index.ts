ff6ce22dc05da16289441a8eb4d5b247f558ff87
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-01-26 11:45:40 +0800

    chore(*): apply assignSubComponent util

--- components/typography/index.ts
+++ components/typography/index.ts
@@ -3,26 +3,18 @@ import Paragraph from './paragraph';
 import Typography from './typography';
 import createTitle from './title';
 import { TypographyProps, TitleProps, TextProps, ParagraphProps } from './types';
+import { assignSubComponent } from '../util/component';
 
-const WithSubTypography = Typography as typeof Typography & {
-    H1: ReturnType<typeof createTitle>;
-    H2: ReturnType<typeof createTitle>;
-    H3: ReturnType<typeof createTitle>;
-    H4: ReturnType<typeof createTitle>;
-    H5: ReturnType<typeof createTitle>;
-    H6: ReturnType<typeof createTitle>;
-    Text: typeof Text;
-    Paragraph: typeof Paragraph;
-};
-
-WithSubTypography.Text = Text;
-WithSubTypography.Paragraph = Paragraph;
-WithSubTypography.H1 = createTitle('h1');
-WithSubTypography.H2 = createTitle('h2');
-WithSubTypography.H3 = createTitle('h3');
-WithSubTypography.H4 = createTitle('h4');
-WithSubTypography.H5 = createTitle('h5');
-WithSubTypography.H6 = createTitle('h6');
+const WithSubTypography = assignSubComponent(Typography, {
+    Text,
+    Paragraph,
+    H1: createTitle('h1'),
+    H2: createTitle('h2'),
+    H3: createTitle('h3'),
+    H4: createTitle('h4'),
+    H5: createTitle('h5'),
+    H6: createTitle('h6'),
+});
 
 export type { TypographyProps, TitleProps, TextProps, ParagraphProps };
 
