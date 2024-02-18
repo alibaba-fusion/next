6328a1d83d152421db59f57256975d81863ca139
lancely <wb-lh589421@alibaba-inc.com>
2024-02-18 10:40:07 +0800

    feat(Step): upgrade docs

--- components/step/types.ts
+++ components/step/types.ts
@@ -2,11 +2,10 @@ import React from 'react';
 import { CommonProps } from '../util';
 
 type HTMLAttributesWeak<T> = Omit<React.HTMLAttributes<T>, 'content' | 'onClick' | 'title'>;
-/**
- * Step.Item
- * @api
- */
 
+/**
+ * @api Step.Item
+ */
 export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, CommonProps {
     /**
      * @skip
@@ -25,39 +24,48 @@ export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, Commo
      * @en direction
      * @skip extend form Step
      */
+
     direction?: string;
     /**
      * @skip
      */
     onResize?: () => void;
+
     /**
      * @skip
      */
     stretch?: boolean;
+
     /**
      * @skip
      */
     labelPlacement?: string;
+
     /**
      * @skip
      */
     readOnly?: boolean;
+
     /**
      * @skip
      */
     parentWidth?: number;
+
     /**
      * @skip
      */
     parentHeight?: number;
+
     /**
      * @skip
      */
     parentLeft?: number;
+
     /**
      * @skip
      */
     parentTop?: number;
+
     /**
      * @skip
      */
@@ -69,9 +77,10 @@ export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, Commo
 
     /**
      * 是否开启动效
-     * @skip
+     * @skip extend for Step
      */
     animation?: boolean;
+
     /**
      * 步骤的状态，如不传，会根据外层的 Step 的 current 属性生成，可选值为 `wait`, `process`, `finish`
      * @en Status of the step, if not passed, it will be generated according to the current attribute of the outer Step, optional values are `wait`, `process`, `finish`
@@ -93,13 +102,13 @@ export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, Commo
     /**
      * 内容填充, shape为 arrow 时无效
      * @en Content, invalid when shape is arrow
-     * @example React.ReactNode
      */
     content?: React.ReactNode;
 
     /**
      * StepItem 的自定义渲染, 会覆盖父节点设置的itemRender
      * @en Custom node render function
+     * @example Function(index: Number, status: String) =\> Node
      * @param index - \{Number\} 节点索引
      * @param status - \{String\} 节点状态
      * @returns  React.ReactNode  -\{Node\} 节点的渲染结果
@@ -113,28 +122,31 @@ export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, Commo
 
     /**
      * 百分比
+     * @en Percent
      */
     percent?: number;
 
     /**
      * 是否禁用
+     * @en disabled
      */
     disabled?: boolean;
 
     /**
      * 点击步骤时的回调
-     * access onClick?: (index: number) =\> unknown;
      * @en Callback when clicking on the step
+     * @example Function(index: Number) =\> void
      * @param index -节点索引
-     * @returns unknown
      * @defaultValue 	() =\> \{\}
      */
     onClick?: (index: number) => unknown;
 
     /**
      * 自定义样式
+     * @skip
      */
     className?: string;
+
     /**
      * @skip  extend form Step
      */
@@ -142,8 +154,7 @@ export interface ItemProps<T = HTMLElement> extends HTMLAttributesWeak<T>, Commo
 }
 
 /**
- * Step
- * @api
+ * @api Step
  */
 export interface StepProps extends Omit<React.HTMLAttributes<HTMLElement>, 'type'>, CommonProps {
     /**
@@ -152,23 +163,6 @@ export interface StepProps extends Omit<React.HTMLAttributes<HTMLElement>, 'type
      * @defaultValue 0
      */
     current?: number;
-    /**
-     * 展示方向
-     * @en Direction
-     * @defaultValue 'ver'
-     */
-    direction?: 'hoz' | 'ver' | string;
-    /**
-     * 宽度是否横向拉伸
-     * @en Stretch the width
-     */
-    stretch?: boolean;
-    /**
-     * 横向布局时( direction 为 hoz )的内容排列
-     * @en Label placement
-     * @defaultValue 'ver'
-     */
-    labelPlacement?: 'hoz' | 'ver' | string;
 
     /**
      * 类型
@@ -176,6 +170,20 @@ export interface StepProps extends Omit<React.HTMLAttributes<HTMLElement>, 'type
      * @defaultValue 'circle'
      */
     shape?: 'circle' | 'arrow' | 'dot';
+
+    /**
+     * 展示方向
+     * @en Direction
+     * @defaultValue 'ver'
+     */
+    direction?: 'hoz' | 'ver' | string;
+
+    /**
+     * 横向布局时( direction 为 hoz )的内容排列
+     * @en Label placement
+     * @defaultValue 'ver'
+     */
+    labelPlacement?: 'hoz' | 'ver' | string;
 
     /**
      * 是否只读模式
@@ -186,12 +194,14 @@ export interface StepProps extends Omit<React.HTMLAttributes<HTMLElement>, 'type
     /**
      * 是否开启动效
      * @en Enable animation
+     * @defaultValue true
      */
     animation?: boolean;
 
     /**
      * 自定义样式名
      * @en Custom class name
+     * @skip
      */
     className?: string;
 
@@ -210,4 +220,10 @@ export interface StepProps extends Omit<React.HTMLAttributes<HTMLElement>, 'type
      * @skip
      */
     type?: 'circle' | 'arrow' | 'dot';
+
+    /**
+     * 宽度是否横向拉伸
+     * @en Stretch the width
+     */
+    stretch?: boolean;
 }
