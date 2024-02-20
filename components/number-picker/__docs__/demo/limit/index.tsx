d19f3247cfac53606e068ddd29e9d921bc68c349
骆林 <luolin.ck@alibaba-inc.com>
2024-02-20 16:18:22 +0800

    refactor(NumberPicker): Upgrade tests and docs, convert to TypeScript

--- components/number-picker/__docs__/demo/limit/index.tsx
+++ components/number-picker/__docs__/demo/limit/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NumberPicker } from '@alifd/next';
+import type { NumberPickerProps } from '@alifd/next/lib/number-picker';
 
-function onChange(value, e) {
+const onChange: NumberPickerProps['onChange'] = (value, e) => {
     console.log('onChange', value, e);
-}
+};
 
-function onCorrect(obj) {
+const onCorrect: NumberPickerProps['onCorrect'] = obj => {
     console.log('onCorrect', obj);
-}
+};
 ReactDOM.render(
     <div>
         <NumberPicker
