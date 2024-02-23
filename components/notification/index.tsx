236394e8199637b11279cb8b6894a9f8d2e06029
暁仙 <xiaoxian.xlf@alibaba-inc.com>
2024-02-23 11:01:49 +0800

    chore(Notification): adjust ts types

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -136,25 +136,27 @@ class Notification extends Component {
                     }}
                     singleMode={false}
                 >
-                    {notifications.map(({ key, type, title, content, icon, onClick, style, className }) => (
-                        <Message
-                            key={key}
-                            shape="toast"
-                            type={type}
-                            title={title}
-                            iconType={icon}
-                            closeable
-                            animation={false}
-                            size={config.size}
-                            visible
-                            style={style}
-                            className={className}
-                            onClick={onClick}
-                            onClose={() => close(key)}
-                        >
-                            {content}
-                        </Message>
-                    ))}
+                    {notifications.map(
+                        ({ key, type, title, content, icon, onClick, style, className }) => (
+                            <Message
+                                key={key}
+                                shape="toast"
+                                type={type}
+                                title={title}
+                                iconType={icon}
+                                closeable
+                                animation={false}
+                                size={config.size}
+                                visible
+                                style={style}
+                                className={className}
+                                onClick={onClick}
+                                onClose={() => close(key)}
+                            >
+                                {content}
+                            </Message>
+                        )
+                    )}
                 </Animate>
             </div>
         );
@@ -171,7 +173,8 @@ let waitOpens = [];
 function open(options = {}) {
     if (!options.title && !options.content) return;
 
-    const duration = !options.duration && options.duration !== 0 ? config.duration : options.duration;
+    const duration =
+        !options.duration && options.duration !== 0 ? config.duration : options.duration;
 
     if (!instance) {
         if (!options.key) {
