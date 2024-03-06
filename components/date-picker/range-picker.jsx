264293ba587af3b6f9235ab1114fa27c4eb81e77
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-03-06 18:02:41 +0800

    feat(DatePicker): improve focus logic

--- components/date-picker/range-picker.jsx
+++ components/date-picker/range-picker.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { polyfill } from 'react-lifecycles-compat';
 import classnames from 'classnames';
@@ -243,6 +243,10 @@ class RangePicker extends Component {
         onVisibleChange: func.noop,
     };
 
+    startDateInputRef = createRef();
+    endDateInputRef = createRef();
+    autoSwitchDateInput = false;
+
     constructor(props, context) {
         super(props, context);
         const { format, timeFormat, dateTimeFormat } = getDateTimeFormat(props.format, props.showTime, props.type);
@@ -289,26 +293,15 @@ class RangePicker extends Component {
         };
     }
 
-    getFormValue = (value) => {
-        return value ? value.format(this.state.dateTimeFormat) : null;
-    }
-
     onValueChange = (values, handler = 'onChange') => {
-        const { startValue, endValue, activeDateInput } = this.state
         let ret;
         if (!values.length || !this.state.inputAsString) {
             ret = values;
         } else {
             ret = [
-                values[0] ? this.getFormValue(values[0]) : null,
-                values[1] ? this.getFormValue(values[1]) : null,
+                values[0] ? values[0].format(this.state.dateTimeFormat) : null,
+                values[1] ? values[1].format(this.state.dateTimeFormat) : null,
             ];
-        }
-        if (this.getFormValue(values[1]) === this.getFormValue(endValue) && activeDateInput === 'endValue') {
-            this.onFocusDateInput('startValue')
-        }
-        if (this.getFormValue(values[0]) === this.getFormValue(startValue) && activeDateInput === 'startValue') {
-            this.onFocusDateInput('endValue')
         }
         this.props[handler](ret);
     };
@@ -331,7 +324,7 @@ class RangePicker extends Component {
 
         switch (active || prevActiveDateInput) {
             case 'startValue': {
-                if (!prevEndValue || value.valueOf() <= prevEndValue.valueOf()) {
+                if (!prevEndValue || this.autoSwitchDateInput) {
                     newState.activeDateInput = 'endValue';
                 }
 
@@ -368,7 +361,7 @@ class RangePicker extends Component {
             }
 
             case 'endValue':
-                if (!prevStartValue) {
+                if (!prevStartValue || this.autoSwitchDateInput) {
                     newState.activeDateInput = 'startValue';
                 }
 
@@ -407,6 +400,11 @@ class RangePicker extends Component {
 
         const newStartValue = 'startValue' in newState ? newState.startValue : prevStartValue;
         const newEndValue = 'endValue' in newState ? newState.endValue : prevEndValue;
+
+        // 每当 input 发生了自动切换，则关闭自动切换
+        if (newState.activeDateInput !== prevActiveDateInput) {
+            this.autoSwitchDateInput = false;
+        }
 
         // 受控状态选择不更新值
         if ('value' in this.props) {
@@ -700,6 +698,28 @@ class RangePicker extends Component {
         return disabledTime;
     };
 
+    enableAutoSwitchDateInput = () => {
+        this.autoSwitchDateInput = true;
+    }
+
+    afterOpen = () => {
+        // autoFocus 逻辑手动处理
+        switch(this.state.activeDateInput) {
+            case 'startValue': {
+                if (this.startDateInputRef.current) {
+                    this.startDateInputRef.current.getInstance().focus();
+                }
+                break;
+            }
+            case 'endValue': {
+                if (this.endDateInputRef.current) {
+                    this.endDateInputRef.current.getInstance().focus();
+                }
+                break;
+            }
+        }
+    }
+
     renderPreview([startValue, endValue], others) {
         const { prefix, className, renderPreview } = this.props;
         const { dateTimeFormat } = this.state;
@@ -837,6 +857,8 @@ class RangePicker extends Component {
                 value={startDateInputValue}
                 onFocus={() => this.onFocusDateInput('startValue')}
                 className={startDateInputCls}
+                ref={this.startDateInputRef}
+                onClick={func.makeChain(this.enableAutoSwitchDateInput, sharedInputProps.onClick)}
             />
         );
 
@@ -848,6 +870,8 @@ class RangePicker extends Component {
                 value={endDateInputValue}
                 onFocus={() => this.onFocusDateInput('endValue')}
                 className={endDateInputCls}
+                ref={this.endDateInputRef}
+                onClick={func.makeChain(this.enableAutoSwitchDateInput, sharedInputProps.onClick)}
             />
         );
 
@@ -1095,13 +1119,14 @@ class RangePicker extends Component {
         return (
             <div {...obj.pickOthers(RangePicker.propTypes, others)} className={classNames}>
                 <PopupComponent
-                    autoFocus
                     align={popupAlign}
                     {...popupProps}
                     followTrigger={followTrigger}
                     disabled={disabled}
                     visible={state.visible}
                     onVisibleChange={this.onVisibleChange}
+                    beforeOpen={func.makeChain(this.enableAutoSwitchDateInput, popupProps && popupProps.beforeOpen)}
+                    afterOpen={func.makeChain(this.afterOpen, popupProps && popupProps.afterOpen)}
                     triggerType={popupTriggerType}
                     container={popupContainer}
                     style={popupStyle}
