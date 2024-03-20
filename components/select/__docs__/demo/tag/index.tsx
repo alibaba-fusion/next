6ddead986e0a6059de96d97b239a29a2ac9dab30
lancely <wb-lh589421@alibaba-inc.com>
2024-03-20 16:53:48 +0800

    feat(Select): select multiple mode suport colorful Tag #3778

--- components/select/__docs__/demo/tag/index.tsx
+++ components/select/__docs__/demo/tag/index.tsx
@@ -15,18 +15,42 @@ const dataSource = [
     },
 ];
 
+const dataSourceColorful = [
+    { value: '10001', label: 'Lucy King', color: 'orange' },
+    { value: 10002, label: 'Lily King', color: 'green' },
+    { value: 10003, label: 'Tom Cat', disabled: true, color: 'blue' },
+    {
+        label: 'Special Group',
+        children: [
+            { value: -1, label: 'FALSE', color: 'purple' },
+            { value: 0, label: 'ZERO', color: 'pink' },
+        ],
+    },
+];
+
 function handleChange(value) {
     console.log(value);
 }
 
 ReactDOM.render(
-    <Select
-        aria-label="tag mode"
-        mode="tag"
-        defaultValue={['10001']}
-        onChange={handleChange}
-        dataSource={dataSource}
-        style={{ width: 300 }}
-    />,
+    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+        <Select
+            aria-label="tag mode"
+            mode="tag"
+            defaultValue={['10001']}
+            onChange={handleChange}
+            dataSource={dataSource}
+            style={{ width: 300, marginRight: 8 }}
+        />
+
+        <Select
+            mode="multiple"
+            showSearch
+            defaultValue={['10001', '10002', '-1']}
+            onChange={handleChange}
+            dataSource={dataSourceColorful}
+            style={{ width: 300 }}
+        />
+    </div>,
     mountNode
 );
