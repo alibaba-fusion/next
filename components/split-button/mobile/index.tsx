b0e83b9b85acce4e57bdd76af071e8d95d8895e1
mario.gk <mario.gk@alibaba-inc.com>
2024-03-22 14:52:14 +0800

    refactor(SplitButton): convert to TypeScript, impove docs and tests

--- components/split-button/mobile/index.tsx
+++ components/split-button/mobile/index.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error meet-react does not export SplitButton
 import { SplitButton as MeetSplitButton } from '@alifd/meet-react';
 import NextSplitButton from '../index';
 
--- components/split-button/types.ts
+++ components/split-button/types.ts
@@ -1,134 +1,166 @@
-/// <reference types="react" />
-
 import React from 'react';
-import { Item, Group, Divider, MenuProps } from '../menu';
 import { CommonProps } from '../util';
-import { ButtonProps } from '../button';
-import { PopupProps } from '../overlay';
+import Menu, { type MenuProps } from '../menu';
+import type { ButtonProps } from '../button';
+import type { PopupProps } from '../overlay';
 
-interface HTMLAttributesWeak extends ButtonProps {
-    onSelect?: any;
-}
+/**
+ * @api SplitButton
+ */
+export interface SplitButtonProps extends Omit<ButtonProps, 'onSelect'>, CommonProps {
+    /**
+     * 主按钮的文案
+     * @en Text in button
+     */
+    label?: React.ReactNode;
 
-export interface SplitButtonProps extends HTMLAttributesWeak, CommonProps {
     /**
      * 按钮的类型
+     * @en Typeo of button
+     * @defaultValue 'normal'
      */
     type?: 'normal' | 'primary' | 'secondary';
 
     /**
      * 按钮组的尺寸
+     * @en Size of button
+     * @defaultValue 'medium'
      */
     size?: 'small' | 'medium' | 'large';
 
     /**
-     * 主按钮的文案
-     */
-    label?: React.ReactNode;
-
-    /**
      * 设置标签类型
+     * @en The html tag to be rendered
      */
     component?: 'button' | 'a';
 
     /**
      * 是否为幽灵按钮
+     * @en Setting ghost button
      */
     ghost?: 'light' | 'dark' | false | true;
 
     /**
+     * 是否禁用
+     * @en Is disabled
+     */
+    disabled?: boolean;
+
+    /**
      * 默认激活的菜单项（用法同 Menu 非受控）
+     * @en The keys of default selected items, as Menu
      */
     defaultSelectedKeys?: string[];
 
     /**
      * 激活的菜单项（用法同 Menu 受控）
+     * @en The keys of selected items
      */
     selectedKeys?: string[];
 
     /**
      * 菜单的选择模式
+     * @en The select mode of menu
      */
     selectMode?: 'single' | 'multiple';
 
     /**
+     * 选择菜单项时的回调，参考 Menu
+     * @en Callback when select the menu，see Menu
+     */
+    onSelect?: MenuProps['onSelect'];
+
+    /**
+     * 点击菜单项时的回调，参考 Menu
+     * @en Callback when click the menu，see Menu
+     */
+    onItemClick?: MenuProps['onItemClick'];
+
+    /**
      * 触发按钮的属性（支持 Button 的所有属性透传）
+     * @en The props of trigger element
      */
     triggerProps?: ButtonProps;
 
     /**
      * 弹层菜单的宽度是否与按钮组一致
+     * @en Whether the popup width is the same as the button
+     * @defaultValue true
      */
     autoWidth?: boolean;
 
     /**
      * 弹层是否显示
+     * @en Visible state of the popup
      */
     visible?: boolean;
 
     /**
      * 弹层默认是否显示
+     * @en Default visible state of the popup
      */
     defaultVisible?: boolean;
 
     /**
      * 弹层显示状态变化时的回调函数
+     * @en Callback when visible state change
+     * @param visible - 弹层显示状态
+     * @param type - 触发弹层显示或隐藏的来源 menuSelect 表示由menu触发； fromTrigger 表示由trigger的点击触发； docClick 表示由document的点击触发
      */
-    onVisibleChange?: (visible: boolean, reason: string) => void;
+    onVisibleChange?: (visible: boolean, type: 'menuSelect' | 'fromTrigger' | 'docClick') => void;
 
     /**
      * 弹层的触发方式
+     * @en Trigger type of popup
+     * @defaultValue 'click'
      */
     popupTriggerType?: 'click' | 'hover';
 
     /**
-     * 弹层对齐方式, 详情见Overlay align
+     * 弹层对齐方式, 详情见 Overlay align
+     * @en Align of popup, @see Overlay doc for detail
      */
     popupAlign?: string;
 
     /**
      * 弹层自定义样式
+     * @en Custom style of popup
      */
     popupStyle?: React.CSSProperties;
 
     /**
      * 弹层自定义样式类
+     * @en Custom className of popup
      */
     popupClassName?: string;
 
     /**
      * 透传给弹层的属性
+     * @en The props of popup
      */
     popupProps?: PopupProps;
 
     /**
-     * 点击菜单项后的回调，同 Menu
+     * 弹层容器，如果是函数需要返回 ref，如果是字符串则是该 DOM 的 id，也可以直接传入 DOM 节点
+     * @en The container of popup
      */
-    onItemClick?: (key: string, item: any, event: React.MouseEvent<HTMLElement>) => void;
-
-    /**
-     * 选择菜单后的回调，同 Menu
-     */
-    onSelect?: (selectedKeys: Array<any>, item: any, extra: any) => void;
-
-    /**
-     * 透传给 Menu 的属性
-     */
-    menuProps?: MenuProps;
-
-    /**
-     * 透传给 左侧按钮 的属性
-     */
-    leftButtonProps?: ButtonProps;
+    popupContainer?: PopupProps['container'];
 
     /**
      * 是否跟随滚动
+     * @en follow Trigger or not
      */
     followTrigger?: boolean;
-}
 
-export default class SplitButton extends React.Component<SplitButtonProps, any> {
-    static Item: typeof Item;
-    static Group: typeof Group;
-    static Divider: typeof Divider;
+    /**
+     * 透传给 Menu 的属性
+     * @en The props of menu
+     */
+    menuProps?: MenuProps & { ref?: (ins: React.ComponentRef<typeof Menu>) => void };
+
+    /**
+     * 透传给 左侧按钮 的属性
+     * @en The props of left button
+     */
+    leftButtonProps?: ButtonProps;
 }
