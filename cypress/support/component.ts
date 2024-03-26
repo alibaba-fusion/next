a4244c57f73c49926bfeaaf565b3d716bb186c4c
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-03-26 17:16:08 +0800

    chore(BuildTool): add cypress rerender util

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,13 +1,22 @@
-import { mount } from 'cypress/react';
+import { cloneElement, type ReactElement } from 'react';
+import { mount, type MountReturn } from 'cypress/react';
 import './commands';
+
+function rerender<Props extends object>(tag: string, nextProps: Props) {
+    return cy.get<MountReturn>(`@${tag.replace(/^@/, '')}`).then(({ component, rerender }) => {
+        return rerender(cloneElement(component as ReactElement, nextProps));
+    });
+}
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Cypress {
         interface Chainable {
             mount: typeof mount;
+            rerender: typeof rerender;
         }
     }
 }
 
 Cypress.Commands.add('mount', mount);
+Cypress.Commands.add('rerender', rerender);
