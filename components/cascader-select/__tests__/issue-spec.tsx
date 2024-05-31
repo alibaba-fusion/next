46b28088ff4a8f117e37ed89ea6a3e4363479113
eternalsky <wsj7552715@hotmail.com>
2024-05-31 14:12:23 +0800

    refactor(CascaderSelect): rename to ts

--- components/cascader-select/__tests__/issue-spec.tsx
+++ components/cascader-select/__tests__/issue-spec.tsx
@@ -23,12 +23,18 @@ const ChinaAreaData = [
             {
                 value: '2974',
                 label: '西安',
-                children: [{ value: '2975', label: '西安市' }, { value: '2976', label: '高陵县' }],
+                children: [
+                    { value: '2975', label: '西安市' },
+                    { value: '2976', label: '高陵县' },
+                ],
             },
             {
                 value: '2980',
                 label: '铜川',
-                children: [{ value: '2981', label: '铜川市' }, { value: '2982', label: '宜君县' }],
+                children: [
+                    { value: '2981', label: '铜川市' },
+                    { value: '2982', label: '宜君县' },
+                ],
             },
         ],
     },
@@ -58,7 +64,7 @@ const ChinaAreaData = [
     },
 ];
 
-describe('CascaderSelect issues', function() {
+describe('CascaderSelect issues', function () {
     this.timeout(1000000);
     let wrapper, root;
     beforeEach(() => {
@@ -92,7 +98,10 @@ describe('CascaderSelect issues', function() {
         wrapper = mount(<Demo />, {
             attachTo: root,
         });
-        assert(root.querySelector('span.next-select-inner em').textContent.trim() === '陕西 / 西安 / 西安市');
+        assert(
+            root.querySelector('span.next-select-inner em').textContent.trim() ===
+                '陕西 / 西安 / 西安市'
+        );
         ref.current.setVisible(true);
         await delay(100);
         assert(isExpanded('陕西', 0, 0, root));
@@ -101,7 +110,10 @@ describe('CascaderSelect issues', function() {
         ref.current.setVisible(false);
         await delay(500);
         ref.current.setValue('3373');
-        assert(root.querySelector('span.next-select-inner em').textContent.trim() === '新疆 / 乌鲁木齐 / 乌鲁木齐市');
+        assert(
+            root.querySelector('span.next-select-inner em').textContent.trim() ===
+                '新疆 / 乌鲁木齐 / 乌鲁木齐市'
+        );
         ref.current.setVisible(true);
         await delay(100);
         assert(isExpanded('新疆', 0, 2, root));
