323a61d5a1405a08a26397f6b29bdecfe0607a93
FairyYang <269291280@qq.com>
2024-07-04 10:28:16 +0800

    refactor(Upload): convert to TypeScript, impove docs and tests, close #4622 (#4841)

--- components/upload/__docs__/demo/oss/index.tsx
+++ components/upload/__docs__/demo/oss/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Upload } from '@alifd/next';
+import { type UploadProps } from '@alifd/next/types/upload';
 
 class App extends React.Component {
-    beforeUpload = (file, options) => {
-        return new Promise((resolve, reject) => {
+    beforeUpload: UploadProps['beforeUpload'] = (file, options) => {
+        return new Promise(resolve => {
             setTimeout(() => {
                 // document: https://help.aliyun.com/document_detail/181756.html?#h2-u6D4Fu89C8u5668u7AEFu76F4u4F20u4EE3u78015
                 // mock ajax to get host/OSSAccessKeyId/policy/signature/key
@@ -36,13 +37,16 @@ class App extends React.Component {
             }, 300);
         });
     };
-    onSuccess = (file, value) => {
+
+    onSuccess = (file: File, value: any) => {
         console.log(file, value);
     };
-    formatter = (res, file) => ({
+
+    formatter: UploadProps['formatter'] = (res, file) => ({
         success: true,
         url: file.tempUrl,
     });
+
     render() {
         return (
             <Upload
