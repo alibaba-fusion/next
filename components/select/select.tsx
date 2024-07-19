6143343752a6ee7149096bcbd2a141f95d388171
王思佳/光弘/朝倉凛 <wsj7552715@hotmail.com>
2024-07-19 12:01:51 +0800

    Revert "fix(Select): Selecting all does not affect options that are already d…"
    This reverts commit 7ddc302262c5294b98646ff49b4ecfc389b8bccc.

--- components/select/select.tsx
+++ components/select/select.tsx
@@ -685,26 +685,13 @@ class Select extends Base<SelectProps, SelectState> {
      */
     handleSelectAll(e: UIEvent<HTMLElement>) {
         e && e.preventDefault();
-        const { value } = this.state;
-
         let nextValues: Exclude<DataSourceItem, ObjectItem>[];
 
-        const enabledData = this.dataStore.getEnableDS().map(item => item.value);
-
-        const disabledSelectedValues = ((value as DataSourceItem[]) || [])
-            .filter(item => {
-                if (typeof item === 'object' && item !== null) {
-                    return !enabledData.includes(item.value);
-                }
-                return !enabledData.includes(item);
-            })
-            .map(item => (typeof item === 'object' && item !== null ? item.value : item));
-
         if (this.selectAllYet) {
-            nextValues = [...disabledSelectedValues];
+            nextValues = [];
         } else {
             nextValues = [
-                ...((value as DataSourceItem[]) || []).map(item => {
+                ...((this.state.value as DataSourceItem[]) || []).map(item => {
                     if (typeof item === 'object' && item !== null) {
                         return item.value;
                     }
